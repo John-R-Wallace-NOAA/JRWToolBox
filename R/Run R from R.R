dac6565ab15495f84f74c50fe595cb495c4edb21
John Wallace <John.Wallace@noaa.gov>
2016-06-22 16:02:00 -0700

    Update Run R from R.R

--- R/Run R from R.R
+++ R/Run R from R.R
@@ -1,18 +1,21 @@
 
+Run.R.From.R <- function() {
+  
+  # Informational only
+  
+  # shell("echo xx = list() > Rcmds.txt")
+  # shell("echo xx[[1]] = 33 >> Rcmds.txt")
+  # shell("echo xx[[2]] = 77 >> Rcmds.txt")
+  # shell("echo save(xx, file = 'Save.Rdata') >> Rcmds.txt")
+  # shell("echo xx >> Rcmds.txt")
 
-  shell("echo xx = list() > Rcmds.txt")
-  shell("echo xx[[1]] = 33 >> Rcmds.txt")
-  shell("echo xx[[2]] = 77 >> Rcmds.txt")
-  shell("echo save(xx, file = 'Save.Rdata') >> Rcmds.txt")
-  shell("echo xx >> Rcmds.txt")
-
-  shell(" W:\\R\\R-2.6.2\\bin\\R.exe --slave --no-site-file --no-init-file -f Rcmds.txt")
-  xx <- load("Save.Rdata")  
-
-
-
-  shell("del Rcmds.txt")
-  shell("del Save.Rdata")
-
+  # shell(" W:\\R\\R-2.6.2\\bin\\R.exe --slave --no-site-file --no-init-file -f Rcmds.txt")
+  # xx <- load("Save.Rdata")  
+  
+  
+  # shell("del Rcmds.txt")
+  # shell("del Save.Rdata")
+  
+}
 
 
