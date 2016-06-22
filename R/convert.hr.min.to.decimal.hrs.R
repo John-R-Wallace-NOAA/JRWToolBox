d1d12b820a7aa8ca6ca347ad0f850cdac424abce
John Wallace <John.Wallace@noaa.gov>
2016-06-22 16:37:10 -0700

    Delete convert.hr.min.to.decimal.hrs.R

--- R/convert.hr.min.to.decimal.hrs.R
+++
@@ -1,8 +0,0 @@
-bar <-
-function(i, n, size = 60, char = ">", prefix = memory.size())
-{
-        num <- round((size * i)/n)
-        catf(prefix, " |", paste(rep(char, num), collapse = ""), paste(rep(" ", size - num), collapse = ""), "|\r", sep = "")
-        invisible()
-}
-
