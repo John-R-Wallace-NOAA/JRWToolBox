f2e9b627bbc7c3d7c318cc19983f105c4e598e13
John Wallace <John.Wallace@noaa.gov>
2016-06-27 14:13:16 -0700

    Update color.unique.R

--- R/color.unique.R
+++ R/color.unique.R
@@ -1,2 +1,3 @@
- [1] "black"           "red"             "green"           "blue"            "cyan"            "purple"          "grey"            "orange"          "hotpink"         "brown"           "darkolivegreen2"
-[12] "darkslategrey"   "deepskyblue1"    "violet"          "cyan"            "magenta"         "lightsalmon"     "gold"           
+ color.unique <- c("black", "red", "green", "blue", "cyan", "purple", "grey", "orange", "hotpink", "brown", "darkolivegreen2",
+"darkslategrey", "deepskyblue1", "violet", "cyan", "magenta", "lightsalmon", "gold") 
+
