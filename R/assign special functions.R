88a1489e82d184806516de284684fad27d319296
John Wallace <John.Wallace@noaa.gov>
2016-06-28 11:54:21 -0700

    Delete assign special functions.R

--- R/assign special functions.R
+++
@@ -1,7 +0,0 @@
-assign("%>>%", function(x, y) { x > y & !is.na(x) })
-assign("%<<%", function(x, y) { x < y & !is.na(x) })
-assign("%>=%", function(x, y) { x >= y & !is.na(x) })
-assign("%<=%", function(x, y) { x <= y & !is.na(x) })
-assign("%r1%", function(e1, e2) ifelse(e1 %% e2 == 0, e2, e1 %% e2))  # Remainder function that gives back the divisor, not zero, when evenly divisible, e.g. 14 %r1% 7 gives 7 not 0
-
-
