ff6bcfba11b9f87f6def85695cb8dc7da1261cc6
John Wallace <John.Wallace@noaa.gov>
2016-06-28 16:43:06 -0700

    Add files via upload

--- R/gitAFile.R
+++ R/gitAFile.R
@@ -1,8 +1,8 @@
 
-gitAFile <- function(URL) {
+gitAFile <- function(URL, type= "function") {
 
-  # Use the raw git name, i.e.: "https://raw.githubusercontent.com/John-R-Wallace/R-ToolBox/master/R/AIC.f.R"
-  # For GitHub see:  https://rawgit.com/
+  # Use the raw git name, i.e.: "https://raw.githubusercontent.com/John-R-Wallace/R-ToolBox/master/R/panel.conf.pred.band.R"
+  # For raw GitHub URL see:  https://rawgit.com/
 
   require(RCurl)
 
@@ -18,12 +18,17 @@ gitAFile <- function(URL) {
        ls.ext(file)
    }
 
-   sink('Object.R')
-   cat(paste(readLines(textConnection(getURL(URL))), collapse = "\n"))
-   sink()
-   on.exit(file.remove('Object.R'))
-   Source('Object.R')
    
+   if(type == "csv") 
+     return(read.csv(textConnection(getURL(URL))))
+
+   if(type == "function") {
+     sink('Object.R')
+     cat(paste(readLines(textConnection(getURL(URL))), collapse = "\n"))
+     sink()
+     on.exit(file.remove('Object.R'))
+     Source('Object.R')
+   }
 }
 
 
