42f6fd46dbe6d020f5d5fb94ca730bb2931a461c
John Wallace <John.Wallace@noaa.gov>
2016-06-28 16:48:00 -0700

    Add files via upload

--- R/gitAFile.R
+++ R/gitAFile.R
@@ -3,6 +3,7 @@ gitAFile <- function(URL, type= "function") {
 
   # Use the raw git name, i.e.: "https://raw.githubusercontent.com/John-R-Wallace/R-ToolBox/master/R/panel.conf.pred.band.R"
   # For raw GitHub URL see:  https://rawgit.com/
+  # csv file download information was from here: http://www.r-bloggers.com/data-on-github-the-easy-way-to-make-your-data-available/
 
   require(RCurl)
 
