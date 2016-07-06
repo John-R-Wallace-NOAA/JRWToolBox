562a0b49aa613a371b3453bdffaa43fa410806cb
John Wallace <John.Wallace@noaa.gov>
2016-07-06 10:43:59 -0700

    Update readInExcelLengthCompsJRW.R

--- R/readInExcelLengthCompsJRW.R
+++ R/readInExcelLengthCompsJRW.R
@@ -4,8 +4,8 @@ readInExcelLengthCompsJRW <- function (file, sheet = "LengthComps", skip = 7) {
         "Year", "Project", "AreaSetIdentifier", "AreaName", "DepthStrataSet", 
         "MinStratumDepth", "MaxStratumDepth", "Length", "NumF", 
         "NumM", "NumUnsexed")
-    xx <- xlsxToR(file, sheet, head=T, skip=skip, ver=F)
-    names(xx) <- nombres
+    out <- xlsxToR(file, sheet, head=T, skip=skip, ver=F)
+    names(out) <- nombres
     cat("\nNOTE: column names have been modified from the Excel Sheet. You may want to verify that they match.\n")
-    xx
+    out
 }
