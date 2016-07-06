d1cfe4493cce3f97730bc839a5e7d3bceaa9b937
John Wallace <John.Wallace@noaa.gov>
2016-07-06 13:07:47 -0700

    Update readInExcelBiomassJRW.R

--- R/readInExcelBiomassJRW.R
+++ R/readInExcelBiomassJRW.R
@@ -5,19 +5,12 @@ readInExcelBiomassJRW <- function (file, sheet = "BiomassAbundance", skip = 6, c
     print(out[1:3, ])
    
     if (is.na(colNames[1])) {
-        nombres <- c("Species", "ScientificName", "SpeciesCode", 
-            "Year", "Project", "StrataAreaVersion", "AreaSetId", 
-            "AreaName", "SouthernLatitude", "NorthernLatitude", 
-            "DepthStrataSet", "MinStratumDepth", "MaxStratumDepth", 
-            "StratumArea", "Biomass", "Abundance", "CpueWeightVar", 
-            "CpueCountVar", "BiomassVar", "CV", "N", "Nbio", 
+        colNames <- c("Species", "ScientificName", "SpeciesCode", "Year", "Project", "StrataAreaVersion", "AreaSetId", 
+            "AreaName", "SouthernLatitude", "NorthernLatitude", "DepthStrataSet", "MinStratumDepth", "MaxStratumDepth", 
+            "StratumArea", "Biomass", "Abundance", "CpueWeightVar", "CpueCountVar", "BiomassVar", "CV", "N", "Nbio", 
             "Npos", "NbioPos")
-        names(out) <- nombres
-        cat("NOTE: column names have been modified from the Excel Sheet. You may want to verify that they match.\n")
-    }
-    else 
-        names(out) <- colNames
-    
+        
+    names(out) <- colNames
     cat("\n\nAfter column name change:\n")
     print(out[1:3, ])
     invisible(out)
