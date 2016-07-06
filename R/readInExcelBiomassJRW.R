22e540794615acb02f5a58f222c4eb9a38004289
John Wallace <John.Wallace@noaa.gov>
2016-07-06 13:02:23 -0700

    Create readInExcelBiomassJRW.R

---
+++ R/readInExcelBiomassJRW.R
@@ -0,0 +1,24 @@
+readInExcelBiomassJRW <- function (file, sheet = "BiomassAbundance", skip = 6, colNames = NA) {
+
+    out <- xlsxToR(file, sheet, head = T, skip = skip, ver = F)
+    cat("\nBefore column name change:\n")
+    print(out[1:3, ])
+   
+    if (is.na(colNames[1])) {
+        nombres <- c("Species", "ScientificName", "SpeciesCode", 
+            "Year", "Project", "StrataAreaVersion", "AreaSetId", 
+            "AreaName", "SouthernLatitude", "NorthernLatitude", 
+            "DepthStrataSet", "MinStratumDepth", "MaxStratumDepth", 
+            "StratumArea", "Biomass", "Abundance", "CpueWeightVar", 
+            "CpueCountVar", "BiomassVar", "CV", "N", "Nbio", 
+            "Npos", "NbioPos")
+        names(out) <- nombres
+        cat("NOTE: column names have been modified from the Excel Sheet. You may want to verify that they match.\n")
+    }
+    else 
+        names(out) <- colNames
+    
+    cat("\n\nAfter column name change:\n")
+    print(out[1:3, ])
+    invisible(out)
+}
