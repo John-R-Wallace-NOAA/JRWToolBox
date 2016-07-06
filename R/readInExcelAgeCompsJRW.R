9ce4c3eab8359bfb87ff3b73cbd0a6f381f97e0d
John Wallace <John.Wallace@noaa.gov>
2016-07-06 09:20:49 -0700

    Create readInExcelAgeCompsJRW.R

---
+++ R/readInExcelAgeCompsJRW.R
@@ -0,0 +1,11 @@
+readInExcelAgeCompsJRW <- function (file, sheet = "AgeComps", skip = 7) 
+{
+    nombres <- c("SpeciesCode", "Species", "Year", "Project", 
+        "AreaName", "MinStratumDepth", "MaxStratumDepth", "Length", 
+        "Age", "NumF", "NumM", "NumUnsexed", "LengthedAgeTally", 
+        "AgeTallyF", "AgeTallyM", "AgeTallyU")
+    out <- xlsxToR(file, sheet, head=T, skip=skip, ver=F)
+    names(xx) <- nombres
+    cat("\nNOTE: column names have been modified from the Excel Sheet. You may want to verify that they match.\n\n")
+    out
+}
