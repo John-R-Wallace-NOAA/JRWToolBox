c6cc5c827cb59211cd11a6db5e31bcd8b4fe83a0
John Wallace <John.Wallace@noaa.gov>
2016-07-08 13:07:29 -0700

    Update showObject.R

--- R/showObject.R
+++ R/showObject.R
@@ -1,13 +1,12 @@
-showObject <- function (x, digits = 1, row = F, col = F, sep = "\t", quote = F, ...) 
-{
-    tmpFile <- tempfile()
-    file.create(tmpFile)
-    
-    if (all(apply(x,2,is.numeric))) 
-        write.table(round(x, digits), tmpFile, row = row, 
-            col = col, sep = sep, quote = quote, ...)
-    else write.table(x, tmpFile, row = row, col = col, sep = sep, 
-        quote = quote, ...)
+showObject <- function (x, fileName = tempfile(), digits = 1, row = F, col = F, sep = "\t", quote = F, pager = "winvi.exe", ...) {
 
-    file.show(tmpFile, pager="winvi.exe")
+    file.create(fileName)
+
+    if (all(apply(x, 2, is.numeric))) 
+        write.table(round(x, digits), fileName, row = row, col = col, 
+            sep = sep, quote = quote, ...)
+    else write.table(x, fileName, row = row, col = col, sep = sep, 
+        quote = quote, ...)
+    file.show(fileName, pager = pager)
 }
+
