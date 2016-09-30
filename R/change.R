8d60c9dbf96b8b4562d20421e4725cfb296d88dd
John Wallace <John.Wallace@noaa.gov>
2016-09-30 12:18:29 -0700

    Update change.R

--- R/change.R
+++ R/change.R
@@ -1,14 +1,19 @@
 
-change <- function(x) {
-
-   tf <- ifelse(regexpr("Mydata", search()) == -1, F, T)
+change <- function (x) 
+{
+    tf <- ifelse(regexpr("Mydata", search()) == -1, F, T)
+    if (any(tf)) {
+        pos <- (1:length(search()))[tf]
+        detach(pos = pos)
+    }
+    if(is.data.frame(x))
+       cat('\nA data frame of size (', nrow(x), ", ", ncol(x), ') has been attached in position 2.\n\n', sep="")
+    else {
+        cat('\nAn object with the following structure has been attached in position 2:\n\n', sep="") 
+        str(x)
+        cat("\n")
+    }
+    attach(x, 2, paste("Mydata:", substring(deparse(substitute(x))[1], 
+        1, 30), sep = ""))
    
-   if(any(tf)) {
-   	pos<-(1:length(search()))[tf]
-   	detach(pos=pos)
-   }
-   
-   attach(x, 2, paste("Mydata:", substring(deparse(substitute(x))[1], 1, 30), sep=""))
-
 }
-
