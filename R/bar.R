65a9b646ff0ea6302e643e5482da2eb3d089b5ec
John Wallace <John.Wallace@noaa.gov>
2016-10-06 11:20:46 -0700

    Update bar.R
    Added an outer index.

--- R/bar.R
+++ R/bar.R
@@ -1,10 +1,10 @@
-bar <- function(i, n, size = 60, char = ">", prefix = ifelse(.Platform$OS.type == "windows", memory.size(), i)) {
-        num <- round((size * i)/n)
-        cat(prefix, " |", paste(rep(char, num), collapse = ""), paste(rep(" ", size - num), collapse = ""),
-                "|\r", sep = "")
-        flush.console()
-        if(i == n)
-          cat("\n\r")
-     }
-
-
+bar <- function (i, n, size = 60, char = ">", OuterIndex = 1, prefix = ifelse(.Platform$OS.type == 
+                 "windows", paste(OuterIndex, ":", memory.size()),  paste(OuterIndex, ":", i))) 
+{
+    num <- round((size * i)/n)
+    cat(prefix, " |", paste(rep(char, num), collapse = ""), paste(rep(" ", 
+        size - num), collapse = ""), "|\r", sep = "")
+    flush.console()
+    if (i == n) 
+        cat("\n\r")
+}
