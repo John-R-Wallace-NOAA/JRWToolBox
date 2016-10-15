789f1a81c0e4683e61b9eeb1a945de07fed76aa0
John Wallace <John.Wallace@noaa.gov>
2016-10-15 11:57:39 -0700

    Update git.sf2.R

--- R/git.sf2.R
+++ R/git.sf2.R
@@ -1,5 +1,3 @@
-git.sf2 <- function(run = TRUE) {
-  sf.name <- gitAFile("https://raw.githubusercontent.com/John-R-Wallace/Scratch/master/sf2.R")
-  if(run)
-    eval(parse(text = sf.name))()
+g2 <- function(run = TRUE) {
+  sf.name <- gitAFile("https://raw.githubusercontent.com/John-R-Wallace/Scratch/master/sf2.R", run = run)
 }
