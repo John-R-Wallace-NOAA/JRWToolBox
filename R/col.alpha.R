050f46192adf0c254c0acd5d2f0c05e212b95bcd
John Wallace <John.Wallace@noaa.gov>
2016-11-10 18:29:00 -0800

    Update col.alpha.R

--- R/col.alpha.R
+++ R/col.alpha.R
@@ -1,11 +1,12 @@
 col.alpha <- function (col, alpha = 0.5) {
-      FUNC <- function(col, alpha = alpha) { 
-             COL <- col2rgb(col)/255
-             rgb(red = COL[1], green = COL[2], blue = COL[3], alpha = alpha)
-       }
-       for ( i in 1:length(col))
-                col[i] <- FUNC(col[i], alpha = alpha[i %r1% length(alpha)])
-       col
+   ' # Remainder function that gives back the divisor, not zero, when evenly divisible, e.g. 14 %r1% 7 gives 7 not 0 '
+    assign("%r1%", function(e1, e2) ifelse(e1 %% e2 == 0, e2, e1 %% e2), pos = 1)
+    ' '
+    FUNC <- function(col, alpha = alpha) {
+        COL <- col2rgb(col)/255
+        rgb(red = COL[1], green = COL[2], blue = COL[3], alpha = alpha)
+    }
+    for (i in 1:length(col)) 
+         col[i] <- FUNC(col[i], alpha = alpha[i %r1% length(alpha)])
+    col
 }
-
-
