4c11fe0b0b9a81c7be19206ce3b10aea7e0c4d34
John Wallace <John.Wallace@noaa.gov>
2017-01-04 16:25:52 -0800

    Update get.object.size.bytes.R

--- R/get.object.size.bytes.R
+++ R/get.object.size.bytes.R
@@ -1,2 +1,10 @@
 
-dfasdf
+
+    get.object.size.bytes <- function(object.name, pos = -1) {
+      '#  Changed from Arni Magnusson code'
+         object <- get(object.name, pos = pos)
+         size <- try(unclass(object.size(object)), silent = TRUE)
+         if (class(size) == "try-error") 
+            size <- 0
+         size
+    }
