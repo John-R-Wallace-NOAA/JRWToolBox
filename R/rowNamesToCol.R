b51831041f107196ce6a9cb63db37c9467b4fe4e
John Wallace <John.Wallace@noaa.gov>
2017-03-08 18:19:15 -0800

    Warning suppression

--- R/rowNamesToCol.R
+++ R/rowNamesToCol.R
@@ -1,11 +1,14 @@
-rowNamesToCol <- function(DF, Name = "RowNames") {
-
-      if(any(is.na(as.numeric(rownames(DF)))))
-           Out <- cbind(rownames(DF), DF)
-       else   
-           Out <- cbind(as.numeric(rownames(DF)), DF)
-      ' '
-      colnames(Out)[1] <- Name
-      JRWToolBox::renum(Out)
+rowNamesToCol <- function (DF, Name = "RowNames") 
+{
+    Opt <- options(warn = -1)
+    on.exit(options(Opt))
+    ' '
+    if (any(is.na(as.numeric(rownames(DF))))) 
+        Out <- cbind(rownames(DF), DF)
+    else Out <- cbind(as.numeric(rownames(DF)), DF)
+    ' '
+    colnames(Out)[1] <- Name
+    JRWToolBox::renum(Out)
 }
+
 
