f65025212852680ba44347912c19192190b8bcbf
John Wallace <John.Wallace@noaa.gov>
2017-04-10 18:36:02 -0700

    Update WCGBTS_Combo_Catch_Wt.R

--- R/WCGBTS_Combo_Catch_Wt.R
+++ R/WCGBTS_Combo_Catch_Wt.R
@@ -1,6 +1,4 @@
-WCGBTS_Combo_Catch_Wt <- function (Species = "Sebastes pinniger", YearRange = c(2003, 
-    2015)) 
-{
+WCGBTS_Combo_Catch_Wt <- function (Species = "Sebastes pinniger", YearRange = c(2003, 2015)) {
     if (!any(installed.packages()[, 1] %in% "jsonlite")) 
         install.packages("jsonlite")
     if (!any(installed.packages()[, 1] %in% "JRWToolBox")) 
@@ -21,46 +19,45 @@ WCGBTS_Combo_Catch_Wt <- function (Species = "Sebastes pinniger", YearRange = c(
     " "
     if (length(YearRange) == 1) 
         YearRange <- c(YearRange, YearRange)
-    Vars <- c("scientific_name", "year", "total_catch_wt_kg", 
-        "cpue_kg_per_ha_der", "vessel", "tow")
+    " "
+    Vars <- c("scientific_name", "year", "total_catch_wt_kg", "vessel", "tow")
     " # Available, but not used: cpue_numbers_per_ha_der, project, date_dim$full_date, performance (as an output column)"
-    " # species and performance=Satisfactory added - went with a year range approach "
+    " # species and performance=Satisfactory added; went with a year range approach for the years to select "
     UrlText <- paste0("https://www.nwfsc.noaa.gov/data/api/v1/source/trawl.catch_fact/selection.json?filters=project=Groundfish%20Slope%20and%20Shelf%20Combination%20Survey,", 
-        "actual_station_design_dim$stn_invalid_for_trawl_date_whid=0,", "performance=Satisfactory,", "depth_ftm>=30,depth_ftm<=700,", "field_identified_taxonomy_dim$scientific_name=", 
-        paste(strsplit(Species, " ")[[1]], collapse = "%20"), 
-        ",date_dim$year>=", YearRange[1], ",date_dim$year<=", 
-        YearRange[2], "&variables=", paste0(Vars, collapse = ","))
+        "actual_station_design_dim$stn_invalid_for_trawl_date_whid=0,", 
+        "performance=Satisfactory,", "depth_ftm>=30,depth_ftm<=700,", 
+        "field_identified_taxonomy_dim$scientific_name=", paste(strsplit(Species, 
+            " ")[[1]], collapse = "%20"), ",date_dim$year>=", 
+        YearRange[1], ",date_dim$year<=", YearRange[2], "&variables=", 
+        paste0(Vars, collapse = ","))
+    " "
     SP <- jsonlite::fromJSON(UrlText)
-    " # SP.Before <<- SP "
-    SP <- rename_columns(SP, newname = c("Year", "cpue", "Vessel", 
-        "Scientific_Name", "Wt_kg", "Tow"))
-    " # SP.After <<- SP "
-    SP$Area_Swept_ha <- SP$Wt_kg/SP$cpue
-    SP$cpue <- NULL
-    SP <- SP[, c("Year", "Vessel", "Tow", "Scientific_Name", 
-        "Wt_kg", "Area_Swept_ha")]
+    SP.Before <<- SP
+    SP <- rename_columns(SP, newname = c("Year", "Vessel","Scientific_Name", "Wt_kg", "Tow"))
+    SP.After <<- SP
+    SP <- SP[, c("Year", "Vessel", "Tow", "Scientific_Name", "Wt_kg")]
     " "
     " # Match SP to all tows to get the zeros "
     " "
-    Vars <- c("year", "vessel", "pass", "tow", "depth_m", "longitude_dd", 
-        "latitude_dd")
-    UrlText <- paste0("https://www.nwfsc.noaa.gov/data/api/v1/source/trawl.catch_fact/selection.json?filters=project=Groundfish%20Slope%20and%20Shelf%20Combination%20Survey,", 
-        "actual_station_design_dim$stn_invalid_for_trawl_date_whid=0,", "performance=Satisfactory,", "depth_ftm>=30,depth_ftm<=700,", "date_dim$year>=", YearRange[1], 
-        ",date_dim$year<=", YearRange[2], "&variables=", paste0(Vars, 
-            collapse = ","))
+    Vars <- c("year", "vessel", "pass", "tow", "depth_m", "longitude_dd", "latitude_dd", "area_swept_ha_der")
+    UrlText <- paste0("https://www.nwfsc.noaa.gov/data/api/v1/source/trawl.operation_haul_fact/selection.json?filters=project=Groundfish%20Slope%20and%20Shelf%20Combination%20Survey,", 
+        "actual_station_design_dim$stn_invalid_for_trawl_date_whid=0,", 
+        "performance=Satisfactory,", "depth_ftm>=30,depth_ftm<=700,", 
+        "date_dim$year>=", YearRange[1], ",date_dim$year<=", 
+        YearRange[2], "&variables=", paste0(Vars, collapse = ","))
     All.Tows <- jsonlite::fromJSON(UrlText)
-    All.Tows <- rename_columns(All.Tows, newname = c("Year", 
-        "Pass", "Vessel", "Tow", "Depth_m", "Longitude_dd", "Latitude_dd"))
+    All.Tows <- rename_columns(All.Tows, newname = c("Year", "Pass", "Vessel", "Tow", "Depth_m", "Longitude_dd", "Latitude_dd", "Area_Swept_ha"))
+    " # There should be no duplicates "
     All.Tows <- All.Tows[!duplicated(paste(All.Tows$Year, All.Tows$Pass, 
         All.Tows$Vessel, All.Tows$Tow)), c("Year", "Pass", "Vessel", 
-        "Tow", "Depth_m", "Longitude_dd", "Latitude_dd")]
+        "Tow", "Depth_m", "Longitude_dd", "Latitude_dd", "Area_Swept_ha")]
     " "
-    Out <- JRWToolBox::match.f(All.Tows, SP, c("Year", "Vessel", 
-        "Tow"), c("Year", "Vessel", "Tow"), c("Scientific_Name", 
-        "Wt_kg", "Area_Swept_ha"))
+    Out <- JRWToolBox::match.f(All.Tows, SP, c("Year", "Vessel", "Tow"), c("Year", "Vessel", "Tow"), c("Scientific_Name", "Wt_kg"))
     Out$Wt_kg[is.na(Out$Wt_kg)] <- 0
-    Out$Area_Swept_ha[is.na(Out$Area_Swept_ha)] <- mean(Out$Area_Swept_ha, 
-        na.rm = T)
+    " # No missing swept areas for 2003-2015 "
+    Out$Area_Swept_ha[is.na(Out$Area_Swept_ha)] <- mean(Out$Area_Swept_ha, trim = 0.05, na.rm = TRUE)
+    " # Scientific Name is missing after the matching when Wt_kg is zero  "
     Out$Scientific_Name <- Species
     sort.f(Out, 1:4)
 }
+
