797c514b5547279b47dfe3f67ba259c228ac859b
John Wallace <John.Wallace@noaa.gov>
2017-05-30 18:21:09 -0700

    Update saveToolbox.R

--- R/saveToolbox.R
+++ R/saveToolbox.R
@@ -1,34 +1,30 @@
-saveToolbox <- function(pos = grep("JRW", search()))
-  { 
-
-        save(list=ls(pos=grep("JRW", search())), file="w:/ALL_USR/JRW/R/library/JRW.tb/JRW.tb.Rdata")
-        cat("\n\n")
-        
-         save(list=ls(pos=pos), 
-          file=paste("w:/ALL_USR/JRW/R/library/JRW.tb/JRW.tb.Rdata", "BACKUP", paste(strsplit(date(), ":")[[1]], ".dmp", collapse =".")))
-        cat("\n\n")
-
-        save(list=ls(pos=pos), 
-           file=paste("C:/BACKUP/JRW.tb/JRW.tb.Rdata", "BACKUP", paste(strsplit(date(), ":")[[1]], ".dmp", collapse =".")))
-        cat("\n\n")
-
-    # ASCII versions
-
-	tmp <- gdata::ll(pos = pos)
-	FUNC <- dimnames(tmp[tmp$Class=='function',])[[1]]
-	print(FUNC); flush.console()
-
-	sink(paste("W:/ALL_USR/JRW/R/library/JRW.tb/TOOLBOX", paste(strsplit(date(), ":")[[1]], collapse ="."), ".txt"))
-	for( i in FUNC) {
-	   cat("\n# *****\n", i, " <- \n")
-           print(get(i, pos = pos)); flush.console()
-	}
-        sink()
-
-	sink(paste("C:/BACKUP/JRW.tb/TOOLBOX", paste(strsplit(date(), ":")[[1]], collapse ="."), ".txt"))
-	for( i in FUNC) {
-	   cat("\n# *****\n", i, " <- \n")
-           print(get(i, pos = pos)); flush.console()
-	}
-        sink()
-  }
+saveToolbox <- function (pos = grep("JRW", search()), Loc = paste0(substring(getwd(),1,1), ":/ALL_USR/JRW/R/library/JRW.tb/JRW.tb.Rdata"),
+                            Loc2 = "C:/BACKUP/JRW.tb/JRW.tb.Rdata") 
+{
+    save(list = JRWToolBox::Ls(pos = grep("JRW", search())), file = Loc)
+    cat("\n\n")
+    save(list = JRWToolBox::Ls(pos = pos), file = paste(Loc, "BACKUP",  paste0(paste0(strsplit(date(), ":")[[1]], collapse = ":"), ".dmp")))
+    cat("\n\n")
+    save(list = JRWToolBox::Ls(pos = pos), file = paste(Loc2, 
+        "BACKUP", paste0(paste0(strsplit(date(), ":")[[1]], collapse = ":"), ".dmp")))
+    cat("\n\n")
+    tmp <- gdata::ll(pos = pos)
+    FUNC <- dimnames(tmp[tmp$Class == "function", ])[[1]]
+    print(FUNC)
+    flush.console()
+    sink(paste(Loc, paste(strsplit(date(), ":")[[1]], collapse = "."), ".txt"))
+    for (i in FUNC) {
+        cat("\n# *****\n", i, " <- \n")
+        print(get(i, pos = pos))
+        flush.console()
+    }
+    sink()
+    sink(paste(Loc2, paste(strsplit(date(), 
+        ":")[[1]], collapse = "."), ".txt"))
+    for (i in FUNC) {
+        cat("\n# *****\n", i, " <- \n")
+        print(get(i, pos = pos))
+        flush.console()
+    }
+    sink()
+}
