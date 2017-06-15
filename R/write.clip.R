97ba827a7ff85fbe06c2e5bb952397ab7ecac660
John Wallace <John.Wallace@noaa.gov>
2017-06-15 14:05:10 -0700

    Create write.clip.R

--- R/write.clip.R
+++ R/write.clip.R
@@ -1,16 +1,18 @@
 
-write.clip <- function (x, digits = 1, sep = "\t", quote = F, Round = T, ...) 
+write.clip <- function (x, digits = 1, sep = "\t", quote = F, Round = T, vectorToColumn = TRUE, ...) 
 {
     require(MASS)
-    if (all(apply(x,2,is.numeric)) & Round) 
-        write.matrix(round(x, digits), "clipboard", sep = sep, ...)
-
+    if(is.null(dim(x)))
+        if(vectorToColumn) x <- matrix(x, ncol = 1) else x <- matrix(x, nrow = 1)
+    if (all(apply(x, 2, is.numeric)) & Round) 
+        write.matrix(round(x, digits), "clipboard", sep = sep, 
+            ...)
     else write.matrix(x, "clipboard", sep = sep, ...)
 }
 
-if(F) {
 
-write.clip.OLD <- function (x, digits = 1, row = F, col = T, sep = "\t", quote = F, Round = T, ...) 
+
+write.table.clip <- function (x, digits = 1, row = F, col = T, sep = "\t", quote = F, Round = T, ...) 
 {
     if (all(apply(x,2,is.numeric)) & Round) 
         write.table(round(x, digits), "clipboard", row = row, col = col, sep = sep, quote = quote, ...)
@@ -18,4 +20,3 @@ write.clip.OLD <- function (x, digits = 1, row = F, col = T, sep = "\t", quote =
     else write.table(x, "clipboard", row = row, col = col, sep = sep, quote = quote, ...)
 }
 
-}
