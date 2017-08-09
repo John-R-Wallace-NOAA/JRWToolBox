d32e2abed9882c19b31099ef666e7577c290905c
John Wallace <John.Wallace@noaa.gov>
2017-08-08 17:40:38 -0700

    Update ellipse.R

--- R/ellipse.R
+++ R/ellipse.R
@@ -1,4 +1,5 @@
 ellipse <- function(cx, cy, rx, ry = rx, theta = 0, facets.num = 40, yaxis = TRUE, pointsOnly = FALSE, fill = FALSE, ...) {
+' 
 # Function to plot an ellipse with center (cx,cy)
 # and major axis/2 along x equal to rx, 
 # major axis/2 along y equal to ry
@@ -10,9 +11,6 @@ ellipse <- function(cx, cy, rx, ry = rx, theta = 0, facets.num = 40, yaxis = TRU
 # y- or x-axis (FALSE), as plotting is generally not square
 # ...so using par(pty="s") will eliminate the need for this.
 #
-# The parameter 'pointsOnly' determines if a plot is
-# added-to (FALSE) or the values of the points are returned
-# in a list (TRUE).
 #
 # Fill is a flag to indicate whether fill in the ellipse.
 #
@@ -20,41 +18,47 @@ ellipse <- function(cx, cy, rx, ry = rx, theta = 0, facets.num = 40, yaxis = TRU
 #     and density.
 #
 # This is an adjustment by Brad Biggerstaff 
-# on 27 April 1999 to the funtion circle() written
+# on 27 April 1999 to the function circle() written
 # by John R. Wallace, as noted below.
-#
+# 
 # The original function was obtained from S-news
 #
 # Brad Biggerstaff (bkb5@cdc.gov)
 # May, 1999
 #
+# All cos() and sin() switched to make correct on 8 Aug 2017. (JRW)
+#
+#
 #   cx, cy, coordinates for centre; r is radius
 #   yaxis = T, radius is correct on the y axis
 #   yaxis = F, radius is correct on the x axis    
-#   DATE WRITTEN:  1994      LAST REVISED:   17 July 1995
+#   DATE WRITTEN:  1994      LAST REVISED:   08 Aug 2017
 #   AUTHOR:  John R. Wallace (John.Wallace@noaa.gov)
 #
 #
-	z <- (seq(0, 360, 360/facets.num) * pi)/180
-	pin <- par()$pin
-	usr <- par()$usr
-	adj <- (pin[2]/pin[1])/((usr[4] - usr[3])/(usr[2] - usr[1]))
-	if(yaxis) {
-		x <- sin(z) * rx * adj
-		y <- cos(z) * ry
-	}
-	else {
-		x <- sin(z) * rx
-		y <- (cos(z) * ry * 1)/adj
-	}
-	xprime <- x * cos(theta) + y * sin(theta) + cx
-	yprime <- y * cos(theta) - x * sin(theta) + cy
-	if(!pointsOnly) {
-		if(fill)
-			density <- -1
-		else density <- 0
-		polygon(xprime, yprime, density = density, ...)
-		invisible()
-	}
-	else list(x = xprime, y = yprime)
+# z converted to be in radians  '
+    z <- (seq(0, 360, 360/facets.num) * pi)/180
+    if(figureAdj) {
+       pin <- par()$pin
+       usr <- par()$usr
+       adj <- (pin[2]/pin[1])/((usr[4] - usr[3])/(usr[2] - usr[1]))
+    } else adj <- 1
+    if (yaxis) {
+        x <- cos(z) * rx * adj
+        y <- sin(z) * ry
+    }
+    else {
+        x <- cos(z) * rx
+        y <- sin(z) * ry/adj
+    }
+    xprime <- y * cos(theta) + x * sin(theta) + cx
+    yprime <- y * sin(theta) - x * cos(theta) + cy
+   
+    if (fill) 
+       density <- -1
+    else density <- 0
+    polygon(xprime, yprime, density = density, ...)
+    
+    invisible(data.frame(x = xprime, y = yprime))
+
 }
