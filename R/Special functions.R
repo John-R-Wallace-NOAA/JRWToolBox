f5dc5a95afb1a7f92f0e41195845ec57ea9777af
John Wallace <John.Wallace@noaa.gov>
2017-09-13 16:43:54 -0700

    Update Special functions.R

--- R/Special functions.R
+++ R/Special functions.R
@@ -1,13 +1,18 @@
-"%>>%" <- function(x, y) { x > y & !is.na(x) }
-"%<<%" <- function(x, y) { x < y & !is.na(x) }
-"%>=%" <- function(x, y) { x >= y & !is.na(x) }
-"%<=%" <- function(x, y) { x <= y & !is.na(x) }
-"%r1%" <- function(e1, e2) { ifelse(e1 %% e2 == 0, e2, e1 %% e2) } # Remainder function that gives back the divisor, not zero, when evenly divisible, e.g. 14 %r1% 7 gives 7 not 0
-# %ino% keeps the order of elements listed in table, %in% gives the order found in x
-"%ino%" <- function(x, table) {
+assign("%>>%", function(x, y) { x > y & !is.na(x) }, pos = 1)
+assign("%<<%", function(x, y) { x < y & !is.na(x) }, pos = 1)
+assign("%>=%", function(x, y) { x >= y & !is.na(x) }, pos = 1)
+assign("%<=%", function(x, y) { x <= y & !is.na(x) }, pos = 1)
+
+# Remainder function that gives back the divisor, not zero, when evenly divisible, e.g. 14 %r1% 7 gives 7 not 0
+assign("%r1%", function(e1, e2) ifelse(e1 %% e2 == 0, e2, e1 %% e2), pos = 1)  
+
+# %ino% keeps the order of elements listed in the argument 'table', %in% gives the order found in x
+assign("%ino%", function (x, table)  {
+    ' # %ino% keeps the order of elements listed in the table argument, %in% gives the order found in x '
     xSeq <- seq(along = x)
     names(xSeq) <- x
     xSeq[as.character(table)]
-}
+  }
+)  
 
 
