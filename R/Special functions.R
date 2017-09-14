679fce98c5cfe46c26e6d5f84751852df907a3d4
John Wallace <John.Wallace@noaa.gov>
2017-09-13 17:03:58 -0700

    Update Special functions.R

--- R/Special functions.R
+++ R/Special functions.R
@@ -1,18 +1,16 @@
 "%>>%" <- function(x, y) { x > y & !is.na(x) }
-assign("%<<%", function(x, y) { x < y & !is.na(x) }, pos = 1)
-assign("%>=%", function(x, y) { x >= y & !is.na(x) }, pos = 1)
-assign("%<=%", function(x, y) { x <= y & !is.na(x) }, pos = 1)
+"%<<%" <- function(x, y) { x < y & !is.na(x) }
+"%>=%" <- function(x, y) { x >= y & !is.na(x) }
+"%<=%" <- function(x, y) { x <= y & !is.na(x) }
 
 # Remainder function that gives back the divisor, not zero, when evenly divisible, e.g. 14 %r1% 7 gives 7 not 0
-assign("%r1%", function(e1, e2) ifelse(e1 %% e2 == 0, e2, e1 %% e2), pos = 1)  
+"%r1%" <- function(e1, e2) { ifelse(e1 %% e2 == 0, e2, e1 %% e2) }
 
 # %ino% keeps the order of elements listed in the argument 'table', %in% gives the order found in x
-assign("%ino%", function (x, table)  {
+"%ino%" <- function (x, table)  {
     ' # %ino% keeps the order of elements listed in the table argument, %in% gives the order found in x '
     xSeq <- seq(along = x)
     names(xSeq) <- x
     xSeq[as.character(table)]
   }
-)  
-
-
+  
