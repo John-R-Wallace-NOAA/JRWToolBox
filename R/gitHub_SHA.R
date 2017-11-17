a269b7fda4acec1b303d42351a66b3d042cd2c6d
John Wallace <John.Wallace@noaa.gov>
2017-11-17 12:19:22 -0800

    Update gitHub_SHA.R

--- R/gitHub_SHA.R
+++ R/gitHub_SHA.R
@@ -1,8 +1,9 @@
-gitHub_SHA <- function(repo, ref = "master") {
-
-   devtools:::remote_sha(lapply(repo, devtools:::github_remote, username = NULL, 
-        ref = "master", subdir = NULL, auth_token = devtools:::github_pat(quiet), 
+gitHub_SHA <- function (repo, ref = "master") 
+{
+    SHA <- devtools:::remote_sha(lapply(repo, devtools:::github_remote, 
+        username = NULL, ref = "master", subdir = NULL, auth_token = devtools:::github_pat(quiet), 
         host = "https://api.github.com")[[1]])
+    cat("\nSHA:", SHA, "from reference:", ref)
+    cat("\n\nCurrent date and time and the call to reinstall this Commit in the future:\n\n", as.character(Sys.time()), "\ndevtools::install_github('", repo, "', ref = '", SHA, "')\n\n", sep="")
 }
-
-		
+	
