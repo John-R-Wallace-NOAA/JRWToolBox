e7f64f375fc1179b1e564755fec1516c077edec7
John Wallace <John.Wallace@noaa.gov>
2017-11-17 14:43:07 -0800

    Update gitHub_SHA.R

--- R/gitHub_SHA.R
+++ R/gitHub_SHA.R
@@ -3,7 +3,13 @@ gitHub_SHA <- function (repo, ref = "master")
     SHA <- devtools:::remote_sha(lapply(repo, devtools:::github_remote, 
         username = NULL, ref = "master", subdir = NULL, auth_token = devtools:::github_pat(quiet), 
         host = "https://api.github.com")[[1]])
-    cat("\nSHA:", SHA, "from reference:", ref)
-    cat("\n\nCurrent date and time and the call to revert to this Commit in the future:\n\n", as.character(Sys.time()), "\ndevtools::install_github('", repo, "', ref = '", SHA, "')\n\n", sep="")
-}
 	
+		
+    cat("\nSHA:", SHA, "from reference:", ref)
+    cat("\n\nCurrent date and time and the call to revert to this Commit in the future:\n\n", 
+        as.character(Sys.time()), "\ndevtools::install_github('", 
+        repo, "', ref = '", SHA, "')\n\n", sep = "")
+		
+	invisible(list(Date = Sys.time(), Call = paste("devtools::install_github('",repo, "', ref = '", SHA, "')", sep = "")))
+		
+}
