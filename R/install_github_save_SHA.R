d77c0a71a309084694db86d5bfd5945ebca8fe1a
John Wallace <John.Wallace@noaa.gov>
2017-11-22 14:41:48 -0800

    Update install_github_save_SHA.R

--- R/install_github_save_SHA.R
+++ R/install_github_save_SHA.R
@@ -1,13 +1,12 @@
-
-
 install_github_save_SHA <- function(repo, ref = 'master')  {
 
        devtools::install_github(repo,  ref = ref)
        currentSHA <- JRWToolBox::gitHub_SHA(repo, ref=ref)
 	   packageName <- devtools:::remote_package_name(lapply(repo, devtools:::github_remote, 
           username = NULL, ref = ref, subdir = NULL, auth_token = devtools:::github_pat(quiet), 
-          host = "https://api.github.com")[[1]])		
-	   save(currentSHA, file = file.path(R.home(), "library", packageName, "currentSHA.Rdata") )
+          host = "https://api.github.com")[[1]])	
+       savePath <- file.path(R.home(), "library", packageName, "currentSHA.Rdata")		  
+	   save(currentSHA, file = savePath)
+	   cat("The current SHA has been saved in: ", savePath, "\n\n")
        invisible(currentSHA)
 }
-
