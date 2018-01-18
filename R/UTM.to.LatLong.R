4cc605d12777adaf3f4ccd1dbbdd9d83c72c1187
John Wallace <John.Wallace@noaa.gov>
2018-01-18 15:51:49 -0800

    Update UTM.to.LatLong.R

--- R/UTM.to.LatLong.R
+++ R/UTM.to.LatLong.R
@@ -1,7 +1,8 @@
 UTM.to.LatLong <- function(UTM, zone = "10T") {
-         lib(sp, require = FALSE)
+         lib(sp,  require = FALSE)
          sputm <- sp::SpatialPoints(UTM, proj4string=CRS(paste0("+proj=utm +zone=", zone, " +datum=WGS84")))
          spgeo <- sp::spTransform(sputm, CRS("+proj=longlat +datum=WGS84"))
-         spgeo@coords
-} 
-
+         OUT <- data.frame(spgeo@coords)
+         names(OUT) <- c('Long', 'Lat')
+         OUT
+}  
