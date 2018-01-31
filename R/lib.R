e8c54f6d9510b2332d6538cf3c0de08ac473dace
John Wallace <John.Wallace@noaa.gov>
2018-01-30 17:29:26 -0800

    Create lib.R

---
+++ R/lib.R
@@ -0,0 +1,38 @@
+lib <- function (Package, Package.Name = NULL, require = TRUE, quiet = TRUE, force = FALSE) 
+{
+
+	
+    if (!(is.character(substitute(Package)))) 
+        Package <- deparse(substitute(Package))
+
+	if(grepl("/", Package)) {
+	     if (any(installed.packages()[, 1] %in% "devtools")) {
+	      	    update.packages("devtools", ask = FALSE)
+	      	 }  else 
+	      	    install.packages("devtools", quiet = quiet)
+         if (!any(installed.packages()[, 1] %in% "devtools"))
+	                     stop(paste('CRAN devtools package is not installed; an attempt to install failed (check for internet access)')) 
+         if (require) 
+               require("devtools", character.only = TRUE)		
+   	
+	     if (!(is.character(substitute(Package.Name)))) 
+             Package.Name <- deparse(substitute(Package.Name))
+		 if(Package.Name == "NULL")
+               Package.Name <- get.subs(Package, "/")[[2]]
+		 devtools::install_github(Package, quiet = quiet, force = force)	  
+		 if (!any(installed.packages()[, 1] %in% Package.Name))
+	               stop(paste("GitHub", Package.Name, 'package is not installed; an attempt to install failed (check for internet access)'))  
+		 if (require) 
+             require(Package.Name, character.only = TRUE)		   
+    } else {
+	    if (any(installed.packages()[, 1] %in% Package)) {
+		    update.packages(Package, ask = FALSE)
+		}  else 
+		    install.packages(Package, quiet = quiet)
+		if (!any(installed.packages()[, 1] %in% Package))
+	               stop(paste("CRAN", Package, 'package is not installed; an attempt to install failed (check for internet access)')) 
+        if (require) 
+           require(Package, character.only = TRUE)				   
+	}
+}
+ 
