ccd038f4884bff40b785cff97dc3a329e9af5cba
John Wallace <John.Wallace@noaa.gov>
2018-02-21 17:10:13 -0800

    Update match.f.R

--- R/match.f.R
+++ R/match.f.R
@@ -1,32 +1,30 @@
-match.f <- function(file, table, findex = 1, tindex = findex, tcol = 2, round. = T, digits=0)
-{
-#
-#   DATE WRITTEN:  ???      LAST REVISED:   7 March 2000
-#   AUTHOR:  John R. Wallace (John.Wallace@noaa.gov)
-#
-        paste.col <- function(x)
-        {
-                if(is.null(dim(x)))
-                        return(paste(as.character(x)))
-                out <- paste(as.character(x[, 1]))
-                for(i in 2:ncol(x)) {
-                        out <- paste(out, as.character(x[, i]))
-                }
-                out
+match.f <- function (file, table, findex = 1, tindex = findex, tcol = NULL, round. = T, digits = 0) {
+    paste.col <- function(x) {
+        if (is.null(dim(x)))
+            return(paste(as.character(x)))
+        out <- paste(as.character(x[, 1]))
+        for (i in 2:ncol(x)) {
+            out <- paste(out, as.character(x[, i]))
         }
-        if(is.null(dim(file))) {
-                dim(file) <- c(length(file), 1)
+        out
+    }
+    if (is.null(dim(file))) {
+        dim(file) <- c(length(file), 1)
+    }
+    if (round.) {
+        for (i in findex) {
+            if (is.numeric(file[, i]))
+                file[, i] <- round(file[, i], digits)
         }
-        if(round.) {
-                for(i in findex) {
-                        if(is.numeric(file[, i]))
-                                file[, i] <- round(file[, i], digits)
-                }
-                for(i in tindex) {
-                        if(is.numeric(table[, i]))
-                                table[, i] <- round(table[, i], digits)
-                }
+        for (i in tindex) {
+            if (is.numeric(table[, i]))
+                table[, i] <- round(table[, i], digits)
         }
-        cbind(file, table[match(paste.col(file[, findex]), paste.col(table[, tindex])), tcol, drop = F])
+    }
+    if (is.null(tcol))
+        tcol <- dimnames(table)[[2]][!(dimnames(table)[[2]] %in%
+            tindex)]
+    cbind(file, table[match(paste.col(file[, findex]), paste.col(table[,
+        tindex])), tcol, drop = F])
 }
 
