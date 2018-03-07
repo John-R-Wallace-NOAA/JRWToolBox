1c645fbf468964b4d72c40dde27faaf7f3c6d9df
John Wallace <John.Wallace@noaa.gov>
2018-03-07 15:22:13 -0800

    Update gitHub_SHA.R

--- R/gitHub_SHA.R
+++ R/gitHub_SHA.R
@@ -1,7 +1,7 @@
 gitHub_SHA <- function (repo, ref = "master") 
 {
     SHA <- devtools:::remote_sha(lapply(repo, devtools:::github_remote, 
-        username = NULL, ref = "master", subdir = NULL, auth_token = devtools:::github_pat(quiet), 
+        username = NULL, ref = ref, subdir = NULL, auth_token = devtools:::github_pat(quiet), 
         host = "https://api.github.com")[[1]])
 	
 		
