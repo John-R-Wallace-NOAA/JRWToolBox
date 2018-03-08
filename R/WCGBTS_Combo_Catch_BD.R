37d228e3e10e1bfedbb8ffb9858d2705bcf5d842
John Wallace <John.Wallace@noaa.gov>
2018-03-08 14:58:37 -0800

    Update WCGBTS_Combo_Catch_BD.R

--- R/WCGBTS_Combo_Catch_BD.R
+++ R/WCGBTS_Combo_Catch_BD.R
@@ -1,5 +1,4 @@
-WCGBTS_Combo_Catch_Wt <- function (Species = "Sebastes pinniger", YearRange = c(2003, 
-    2015), verbose = FALSE) 
+WCGBTS_Combo_Catch_BD <- function (Species = "Sebastes pinniger", YearRange = c(2003, 3000), verbose = T) 
 {
     if (!any(installed.packages()[, 1] %in% "jsonlite")) 
         install.packages("jsonlite")
@@ -10,6 +9,8 @@ WCGBTS_Combo_Catch_Wt <- function (Species = "Sebastes pinniger", YearRange = c(
     " # The updated function no longer requires the order of the names inside of newname and origname to be the same. "
     " "
     rename_columns = function(DF, origname = colnames(DF), newname) {
+        " # 'age_years' has both age and years, first forcing a change to 'age' "
+        colnames(DF)[grep("age_years", colnames(DF))] <- "age"
         DF_new = DF
         for (i in 1:length(newname)) {
             Match = grep(newname[i], origname, ignore.case = TRUE)
@@ -22,10 +23,10 @@ WCGBTS_Combo_Catch_Wt <- function (Species = "Sebastes pinniger", YearRange = c(
     if (length(YearRange) == 1) 
         YearRange <- c(YearRange, YearRange)
     " "
-    Vars <- c("scientific_name", "year", "total_catch_wt_kg", "vessel", "tow")
-    " # Available, but not used: cpue_numbers_per_ha_der, project, performance (as an output column)"
-    " # species and performance=Satisfactory added; went with a year range approach for the years to select "
-    UrlText <- paste0("https://www.nwfsc.noaa.gov/data/api/v1/source/trawl.catch_fact/selection.json?filters=project=Groundfish%20Slope%20and%20Shelf%20Combination%20Survey,", 
+    Vars <- c("scientific_name", "year", "vessel", "pass", "tow", "date_dim$full_date", "depth_ftm", "weight_kg", "length_cm", "sex", "age_years", "latitude_dd", "longitude_dd")
+    " # Available, but not used: project, performance (not output, only used as a filter below)  "
+    " # species and performance=Satisfactory added; went with a year range approach for the years to select  "
+    UrlText <- paste0("https://www.nwfsc.noaa.gov/data/api/v1/source/trawl.individual_fact/selection.json?filters=project=Groundfish%20Slope%20and%20Shelf%20Combination%20Survey,", 
         "actual_station_design_dim$stn_invalid_for_trawl_date_whid=0,", 
         "performance=Satisfactory,", "depth_ftm>=30,depth_ftm<=700,", 
         "field_identified_taxonomy_dim$scientific_name=", paste(strsplit(Species, 
@@ -33,47 +34,19 @@ WCGBTS_Combo_Catch_Wt <- function (Species = "Sebastes pinniger", YearRange = c(
         YearRange[1], ",date_dim$year<=", YearRange[2], "&variables=", 
         paste0(Vars, collapse = ","))
     " "
-    if (verbose) 
-        cat("\n\nURL Text for the species:\n\n", UrlText, "\n")
+    if (verbose) cat("\n\nURL Text for the species:\n\n", UrlText, "\n\n")
     " "
     SP <- jsonlite::fromJSON(UrlText)
-    if (verbose) 
-        print(SP[1:4, ])
-    cat("\n\n")
-    "  # SP.Before <<- SP  "
-    SP <- rename_columns(SP, newname = c("Year", "Vessel", "Scientific_Name", "Total_sp_wt_kg", "Tow"))
-    if (verbose) 
-        print(SP[1:4, ])
-    cat("\n\n")
-    " # SP.After <<- SP  "
-    SP <- SP[, c("Year", "Vessel", "Tow", "Scientific_Name", "Total_sp_wt_kg")]
+    if(verbose) print(SP[1:4,]); cat("\n\n")
+    " # SP.Before <<- SP  "
     " "
-    " # Match SP to all tows to get the zeros "
+    SP <- rename_columns(SP, newname = c("Year", "Vessel", "Pass", "Scientific_Name", "Tow", "Date", "Depth_ftm", "Weight_kg", "Length_cm", "Sex", "Age", "Latitude_dd", "Longitude_dd"))
+    if(verbose) print(SP[1:4,]); cat("\n\n")
+    "  # SP.After <<- SP  "
     " "
-    Vars <- c("year", "vessel", "pass", "tow", "date_dim$full_date", "depth_m", "longitude_dd", "latitude_dd", "area_swept_ha_der")
-    UrlText <- paste0("https://www.nwfsc.noaa.gov/data/api/v1/source/trawl.operation_haul_fact/selection.json?filters=project=Groundfish%20Slope%20and%20Shelf%20Combination%20Survey,", 
-        "actual_station_design_dim$stn_invalid_for_trawl_date_whid=0,", 
-        "performance=Satisfactory,", "depth_ftm>=30,depth_ftm<=700,", 
-        "date_dim$year>=", YearRange[1], ",date_dim$year<=", 
-        YearRange[2], "&variables=", paste0(Vars, collapse = ","))
+    SP <- SP[, c("Year", "Vessel", "Pass", "Tow", "Date", "Depth_ftm", "Scientific_Name", "Weight_kg", "Length_cm", "Sex", "Age", "Latitude_dd", "Longitude_dd")]
+    SP$Date <- chron(format(as.POSIXlt(SP$Date, format = "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d"), format = "y-m-d", out.format = "YYYY-m-d")
     " "
-    if (verbose) 
-        cat("\n\nURL Text for all tows (needed for zero catch tows):\n\n", 
-            UrlText, "\n\n")
-    " "
-    All.Tows <- jsonlite::fromJSON(UrlText)
-    All.Tows <- rename_columns(All.Tows, newname = c("Year", "Pass", "Vessel", "Tow", "Date", "Depth_m", "Longitude_dd", "Latitude_dd", "Area_Swept_ha"))
-    " # There should be no duplicates "
-    All.Tows <- All.Tows[!duplicated(paste(All.Tows$Year, All.Tows$Pass, 
-        All.Tows$Vessel, All.Tows$Tow)), c("Year", "Pass", "Vessel", "Tow",  "Date", "Depth_m", "Longitude_dd", "Latitude_dd", "Area_Swept_ha")]
-    " # Note that tow number is within vessel and within year, but not within pass (the Noahs Ark did both passes in 2012 and the tow number max is ~ twice the one pass per year total)  "
-    Out <- JRWToolBox::match.f(All.Tows, SP, c("Year", "Vessel", "Tow"), c("Year", "Vessel", "Tow"), c("Scientific_Name", "Total_sp_wt_kg"))
-    Out$Wt_kg[is.na(Out$Wt_kg)] <- 0
-    " # No missing swept areas for 2003-2015 "
-    Out$Area_Swept_ha[is.na(Out$Area_Swept_ha)] <- mean(Out$Area_Swept_ha, 
-        trim = 0.05, na.rm = TRUE)
-    " # Scientific Name is missing after the matching when Wt_kg is zero  "
-    Out$Scientific_Name <- Species
-	Out$Date <- chron(format(as.POSIXlt(Out$Date, format = "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d"), format = "y-m-d", out.format = "YYYY-m-d")
-    invisible(sort.f(Out, 1:4))
+    if(verbose) print(SP[1:4,]); cat("\n\n")
+    invisible(SP)
 }
