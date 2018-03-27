186b58faef49f6d1e555bc36ca4ec788af48f2b2
John Wallace <John.Wallace@noaa.gov>
2018-03-27 13:52:33 -0700

    Matrix objects with duplicate row names now work

--- R/r.R
+++ R/r.R
@@ -1,15 +1,20 @@
-r <- function(x, digits = 7)
+r <- function (x, digits = 7) 
 {
-       if(is.null(dim(x)))
-               if(is.numeric(x))
-                       round(x, digits=digits)
-               else x
-       else { 
-               out <- data.frame(lapply(data.frame(x), function(y)
-                    if(is.numeric(y)) round(y, digits=digits) else y))
-               colnames(out) <- colnames(x)
-               rownames(out) <- rownames(x)
-               out
-       }
+    if (is.null(dim(x))) {
+        if (is.numeric(x)) 
+            round(x, digits = digits)
+        else x
+    } else {
+	    onlyMatrix <- is.matrix(x) & !is.data.frame(x)
+		RowNames <- rownames(x)
+		x <- JRWToolBox::renum(x)
+        out <- data.frame(lapply(data.frame(x), function(y) if (is.numeric(y)) {
+            round(y, digits = digits) } else y))
+		if(onlyMatrix)
+		    out <- as.matrix(out) 
+        colnames(out) <- colnames(x)
+		rownames(out) <- rowNames
+		out
+    }
 }
 
