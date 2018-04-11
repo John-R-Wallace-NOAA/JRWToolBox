593a0a8519fc9d133c52624fbd2ee9aba78f11aa
John Wallace <John.Wallace@noaa.gov>
2018-04-10 17:53:34 -0700

    Update SHA.R

--- R/SHA.R
+++ R/SHA.R
@@ -1,7 +1,8 @@
-	 
 SHA <- function (repo) 
 {
-    SHA <- packageDescription("multidplyr")$RemoteSha
+    SHA <- packageDescription(repo)$RemoteSha
+	if(is.null(SHA))
+	    stop("No SHA number. Perhaps a CRAN package, not one downloaded from GitHub.")
     cat("\nSHA:", SHA)
     cat("\n\nCurrent date and time and the call to revert to this Commit in the future:\n\n", 
         as.character(Sys.time()), "\ndevtools::install_github('", 
