33f8fb945eba2326a643f4a367aa7bc035a118c5
John Wallace <John.Wallace@noaa.gov>
2018-04-10 17:54:36 -0700

    Update lib.R

--- R/lib.R
+++ R/lib.R
@@ -1,41 +1,45 @@
-lib <- function (Package, Package.Name = NULL, require = TRUE, quiet = TRUE, force = FALSE, autoAddRepo = TRUE) 
+lib <- function (Package, Package.Name = NULL, require = TRUE, quiet = TRUE, 
+    force = FALSE, autoAddRepo = TRUE) 
 {
- 
-if(autoAddRepo & options()$repos[names(options()$repos) %in% 'CRAN'] %in% "@CRAN@") 	 
-       local({getOption("repos") -> r; r["CRAN"] <- "http://cran.fhcrc.org"; options(repos = r)})	 
-'  '
-	
+    if (autoAddRepo & options()$repos[names(options()$repos) %in% 
+        "CRAN"] %in% "@CRAN@") 
+        local({
+            r <- getOption("repos")
+            r["CRAN"] <- "http://cran.fhcrc.org"
+            options(repos = r)
+        })
+    "  "
     if (!(is.character(substitute(Package)))) 
         Package <- deparse(substitute(Package))
-
-	if(grepl("/", Package)) {
-	     if (any(installed.packages()[, 1] %in% "devtools")) {
-	      	    update.packages("devtools", ask = FALSE)
-	      	 }  else 
-	      	    install.packages("devtools", quiet = quiet)
-         if (!any(installed.packages()[, 1] %in% "devtools"))
-	                     stop(paste('CRAN devtools package is not installed; an attempt to install failed (check for internet access)')) 
-         if (require) 
-               require("devtools", character.only = TRUE)		
-   	
-	     if (!(is.character(substitute(Package.Name)))) 
-             Package.Name <- deparse(substitute(Package.Name))
-		 if(Package.Name == "NULL")
-               Package.Name <- get.subs(Package, "/")[[2]]
-		 devtools::install_github(Package, quiet = quiet, force = force)	  
-		 if (!any(installed.packages()[, 1] %in% Package.Name))
-	               stop(paste("GitHub", Package.Name, 'package is not installed; an attempt to install failed (check for internet access)'))  
-		 if (require) 
-             require(Package.Name, character.only = TRUE)		   
-    } else {
-	    if (any(installed.packages()[, 1] %in% Package)) {
-		    update.packages(Package, ask = FALSE)
-		}  else 
-		    install.packages(Package, quiet = quiet)
-		if (!any(installed.packages()[, 1] %in% Package))
-	               stop(paste("CRAN", Package, 'package is not installed; an attempt to install failed (check for internet access)')) 
+    if (grepl("/", Package)) {
+        if (any(installed.packages()[, 1] %in% "devtools")) {
+            update.packages("devtools", ask = FALSE)
+        } else install.packages("devtools", quiet = quiet)
+        if (!any(installed.packages()[, 1] %in% "devtools")) 
+            stop(paste("CRAN devtools package is not installed; an attempt to install failed (check for internet access)"))
         if (require) 
-           require(Package, character.only = TRUE)				   
-	}
+            require("devtools", character.only = TRUE)
+        if (!(is.character(substitute(Package.Name)))) 
+            Package.Name <- deparse(substitute(Package.Name))
+        if (Package.Name == "NULL") 
+            Package.Name <- get.subs(Package, "/")[[2]]
+        devtools::install_github(Package, quiet = quiet, force = force)
+        if (!any(installed.packages()[, 1] %in% Package.Name)) 
+            stop(paste0("R '", Package.Name, "' package from Github is not installed. Note that the R package name may not be the same 
+			        as the GitHub directory name, if so, use the Package.Name argument. Find the R package name using quiet = FALSE."))
+        if (require) 
+            require(Package.Name, character.only = TRUE)
+    }
+    else {
+        if (any(installed.packages()[, 1] %in% Package)) {
+            update.packages(Package, ask = FALSE)
+        }
+        else install.packages(Package, quiet = quiet)
+        if (!any(installed.packages()[, 1] %in% Package)) 
+            stop(paste("CRAN", Package, "package is not installed; an attempt to install failed (check for internet access)"))
+        if (require) 
+            require(Package, character.only = TRUE)
+    }
 }
- 
+
+
