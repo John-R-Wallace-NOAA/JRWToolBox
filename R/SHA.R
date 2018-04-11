47bcf5170694e4ddac016996409a9ff3bb442cce
John Wallace <John.Wallace@noaa.gov>
2018-04-10 18:29:29 -0700

    Update SHA.R

--- R/SHA.R
+++ R/SHA.R
@@ -1,12 +1,12 @@
-SHA <- function (repo) 
+HA <- function (repo) 
 {
     SHA <- packageDescription(repo)$RemoteSha
-	if(is.null(SHA))
-	    stop("No SHA number. Perhaps a CRAN package, not one downloaded from GitHub.")
+    if (is.null(SHA)) 
+        stop("No SHA number. Perhaps a CRAN package, not one downloaded from GitHub.")
     cat("\nSHA:", SHA)
     cat("\n\nCurrent date and time and the call to revert to this Commit in the future:\n\n", 
-        as.character(Sys.time()), "\ndevtools::install_github('", 
-        repo, "', ref = '", SHA, "')\n\n", sep = "")
+        as.character(Sys.time()), "\ndevtools::install_github('", packageDescription(repo)$RemoteUsername, "/", 
+		packageDescription(repo)$GithubRepo, "', ref = '", SHA, "')\n\n", sep = "")
     invisible(list(Date = Sys.time(), Call = paste("devtools::install_github('", 
         repo, "', ref = '", SHA, "')", sep = "")))
 }
