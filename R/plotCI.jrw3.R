0832b8cb06eabdb71ff3ccf3022c096bd55ea010
John Wallace <John.Wallace@noaa.gov>
2018-05-08 14:12:54 -0700

    Update plotCI.jrw3.R

--- R/plotCI.jrw3.R
+++ R/plotCI.jrw3.R
@@ -1,6 +1,6 @@
-plotCI.jrw3 <- 
-function (x, y = NULL, uiw, liw = uiw, ylo = NULL, yhi = NULL, 
-    sfrac = 0.01, ymax = NULL, ylim=NULL, add = FALSE, maxValue = NULL, col = "black", ...) 
+plotCI.jrw3 <- function (x, y = NULL, uiw, liw = uiw, ylo = NULL, yhi = NULL, 
+    sfrac = 0.01, ymax = NULL, ylim = NULL, add = FALSE, maxValue = NULL, 
+    col = "black", lwd = 1, ...) 
 {
     if (is.list(x)) {
         y <- x$y
@@ -13,21 +13,19 @@ function (x, y = NULL, uiw, liw = uiw, ylo = NULL, yhi = NULL,
         y <- as.numeric(x)
         x <- seq(along = x)
     }
-    if(is.null(maxValue))
-         ui <- y + uiw
-    else
-         ui <- ifelse(y + uiw > maxValue, maxValue, y + uiw)
+    if (is.null(maxValue)) 
+        ui <- y + uiw
+    else ui <- ifelse(y + uiw > maxValue, maxValue, y + uiw)
     li <- y - liw
-    if(is.null(ylim))
+    if (is.null(ylim)) 
         ylim <- range(c(y, ui, li, ylo, yhi, ymax))
     if (!add) 
-        plot(x, y, ylim = ylim, col = col, ...)
+        plot(x, y, ylim = ylim, col = col, lwd = lwd, ...)
     else points(x, y, col = col, ...)
     smidge <- diff(par("usr")[1:2]) * sfrac
-    segments(x, li, x, ui, col = col)
+    segments(x, li, x, ui, col = col, lwd = lwd)
     x2 <- c(x, x)
     ul <- c(li, ui)
-    segments(x2 - smidge, ul, x2 + smidge, ul, col = col)
+    segments(x2 - smidge, ul, x2 + smidge, ul, col = col, lwd = lwd)
     invisible(list(x = x, y = y))
 }
-
