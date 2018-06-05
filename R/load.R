62040e7b710e890b2e4911f1d0f29d3348be0ff5
John Wallace <John.Wallace@noaa.gov>
2018-06-05 16:30:59 -0700

    Update load.R

--- R/load.R
+++ R/load.R
@@ -1,18 +1,21 @@
- load <- function (file, str. = TRUE, list.len = 15, nrow = 5, ...) 
+load <- function (file, str. = TRUE, list.len = 15, nrow = 5, ...) 
 {
+  '  # The existence of baseLoad is a flag for doing base::load() - used for Windows auto load on drag and drop  '
     ls.ext <- function(file, str. = TRUE, list.len, nrow) {
         local({
             base::load(file)
             if (str. == TRUE) {
                 Names <- base::ls()
-                for (i in Names[-grep('%', Names)]) {
-                   OBJ <- eval(parse(text = i))
-                   cat("\n", i, ":\n\n", sep = "")
-                   str(OBJ, list.len = list.len)
-                   cat("\n")
-                   if(is.matrix(OBJ) | is.data.frame(OBJ)) { 
-                      print(head(OBJ, nrow)); flush.console()
-                      cat("\nDimension:", dim(OBJ), "\n\n"); flush.console()
+                for (i in Names[-grep("%", Names)]) {
+                  OBJ <- eval(parse(text = i))
+                  cat("\n", i, ":\n\n", sep = "")
+                  str(OBJ, list.len = list.len)
+                  cat("\n")
+                  if (is.matrix(OBJ) | is.data.frame(OBJ)) {
+                    print(head(OBJ, nrow))
+                    flush.console()
+                    cat("\nDimension:", dim(OBJ), "\n\n")
+                    flush.console()
                   }
                 }
                 rm(i, Names)
@@ -22,8 +25,6 @@
         })
     }
     base::load(file, .GlobalEnv, ...)
-    ls.ext(file, str. = str., list.len = list.len, nrow = nrow)
+    if(!exists('baseLoad'))
+       ls.ext(file, str. = str., list.len = list.len, nrow = nrow)
 }
-
-
-
