265c7f4aaec8344b5a3325043b1b3f5aa020aa6b
John Wallace <John.Wallace@noaa.gov>
2018-06-08 15:15:51 -0700

    Update askNum.R

--- R/askNum.R
+++ R/askNum.R
@@ -1,7 +1,9 @@
-askNum <- function (msg, verify = TRUE) 
+askNum <- function (msg, decimalPoints = 0, verify = TRUE) 
 {
+    dev.new()
     while (TRUE) {
-        cat("\n\n", msg, "\n\n"); flush.console()
+        cat("\n\n", msg, "\n\n")
+        flush.console()
         plot(0:9, rep(5, 10), xlab = "0 - 9", ylab = "")
         text(3, 6, "Enter each digit in the number by clicking on the circles below:")
         Num <- NULL
@@ -18,7 +20,8 @@ askNum <- function (msg, verify = TRUE)
             NULL
         else Num
         if (verify) {
-            cat("\nIs this the correct number: ", Num, "\nClick zero for FALSE or 1-9 for TRUE\n"); flush.console()
+            cat("\nIs this the correct number: ", Num/10^decimalPoints, "\nClick zero for FALSE or 1-9 for TRUE\n")
+            flush.console()
             plot(0:9, rep(5, 10))
             text(3, 6, "Click zero for FALSE or 1-9 for TRUE")
             Pos <- locator(1)
@@ -26,11 +29,14 @@ askNum <- function (msg, verify = TRUE)
             text(Pos, label = ifelse(N == 0, "FALSE", "TRUE"))
             if (N) 
                 break
-            cat("\nTry again\n"); flush.console()
+            cat("\nTry again\n")
+            flush.console()
             timer(2, silent = TRUE)
         }
         else break
     }
-	dev.off()
-    Num
+    dev.off()
+    Num/10^decimalPoints
 }
+
+
