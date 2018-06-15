c5decdf775f4614ba29fee9683567b87e1bb19a3
John Wallace <John.Wallace@noaa.gov>
2018-06-15 16:37:48 -0700

    Update install_github_save_SHA.R

--- R/install_github_save_SHA.R
+++ R/install_github_save_SHA.R
@@ -1,12 +1,14 @@
-install_github_save_SHA <- function(repo, ref = 'master')  {
-
-       devtools::install_github(repo,  ref = ref)
-       currentSHA <- JRWToolBox::gitHub_SHA(repo, ref=ref)
-	   packageName <- devtools:::remote_package_name(lapply(repo, devtools:::github_remote, 
-          username = NULL, ref = ref, subdir = NULL, auth_token = devtools:::github_pat(quiet), 
-          host = "https://api.github.com")[[1]])	
-       savePath <- file.path(R.home(), "library", packageName, "currentSHA.Rdata")		  
-	   save(currentSHA, file = savePath)
-	   cat("The current SHA has been saved in: ", savePath, "\n\n")
-       invisible(currentSHA)
+install_github_save_SHA <- function (repo, ref = "master", ...) 
+{
+    devtools::install_github(repo, ref = ref, ...)
+    currentSHA <- JRWToolBox::SHA(get.subs(repo, "/")[[length(get.subs(repo, "/"))]])
+    packageName <- devtools:::remote_package_name(lapply(repo, 
+        devtools:::github_remote, username = NULL, ref = ref, 
+        subdir = NULL, auth_token = devtools:::github_pat(quiet), 
+        host = "https://api.github.com")[[1]])
+    savePath <- file.path(R.home(), "library", packageName, "currentSHA.Rdata")
+    save(currentSHA, file = savePath)
+    cat("The current SHA has been saved in: ", savePath, "\n\n")
+    invisible(currentSHA)
 }
+
