7820a1a2c840764c74d86f53f443a78594348770
John Wallace <John.Wallace@noaa.gov>
2018-08-02 16:33:56 -0700

    stringsAsFactors =FALSE

--- R/scanIn.R
+++ R/scanIn.R
@@ -1,5 +1,5 @@
 scanIn <- function(text, header = TRUE, ncol = 2) {
-    Out <- data.frame(matrix(scan(what = "", text = text, quiet = TRUE), ncol = ncol, byrow= TRUE))
+    Out <- data.frame(matrix(scan(what = "", text = text, quiet = TRUE), ncol = ncol, byrow= TRUE), stringsAsFactors = FALSE)
     if(header) {
        names(Out) <- as.character(Out[1,])
 	     Out <- Out[-1,]
