a8183f21a4c05ab2827c246ddb665dc95efe7900
John Wallace <John.Wallace@noaa.gov>
2018-08-02 16:20:16 -0700

    Debugged

--- R/scanIn.R
+++ R/scanIn.R
@@ -1,7 +1,7 @@
 scanIn <- function(text, header = TRUE, ncol = 2) {
     Out <- data.frame(matrix(scan(what = "", text = text, quiet = TRUE), ncol = ncol, byrow= TRUE))
     if(header) {
-       names(Out) <- Out[1,]
+       names(Out) <- as.character(Out[1,])
 	     Out <- Out[-1,]
     }
     JRWToolBox::renum(Out)
