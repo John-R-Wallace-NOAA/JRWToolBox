a0bb686b00dfffc99523da35b24378f43044522f
John Wallace <John.Wallace@noaa.gov>
2018-08-10 10:55:20 -0700

    Update Special functions.R

--- R/Special functions.R
+++ R/Special functions.R
@@ -9,6 +9,7 @@
 # %ino% keeps the order of elements listed in the argument 'table', %in% gives the order found in x
 "%ino%" <- function (x, table)  {
     "  # %ino% keeps the order of elements listed in the table argument, %in% gives the order found in x  "
+    "  # https://stackoverflow.com/questions/10586652/r-preserve-order-when-using-matching-operators-in  "
     "  # LETTERS[LETTERS %in% c('Z', 'C' 'A')]  => [1] 'A' 'C' 'Z'  "
     "  # LETTERS[LETTERS %ino% c('Z', 'C', 'A')]  => [1] 'Z' 'C' 'A' "
 
