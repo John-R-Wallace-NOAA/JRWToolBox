7dacc34ba5c55d1c7c48b24f1fcb9c7f1ddfcdf5
John Wallace <John.Wallace@noaa.gov>
2018-09-27 14:11:21 -0700

    Replacement for graphics::rect()

---
+++ R/rectangle.R
@@ -0,0 +1,7 @@
+rectangle <- function (xleft, ybottom, xright, ytop, ...) {
+    ' #  graphics:rect() doesn't close fully in the upper left-hand corner with lwd = 2 '
+    lines(c(xleft, xleft, xright, xright, xleft), c(ybottom, ytop, ytop, ybottom, ybottom), ...)
+
+}    
+ 
+
