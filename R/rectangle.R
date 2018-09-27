eb969be0abef2ca5adcf3852acb848cfa323cd0f
John Wallace <John.Wallace@noaa.gov>
2018-09-27 14:13:28 -0700

    bug fix

--- R/rectangle.R
+++ R/rectangle.R
@@ -1,5 +1,5 @@
 rectangle <- function (xleft, ybottom, xright, ytop, ...) {
-    ' #  graphics:rect() doesn't close fully in the upper left-hand corner with lwd = 2 '
+    ' #  graphics::rect() doesn't close fully in the upper left-hand corner with lwd = 2 '
     lines(c(xleft, xleft, xright, xright, xleft), c(ybottom, ytop, ytop, ybottom, ybottom), ...)
 
 }    
