f7a0c5924932c5d8b564660722a4c6858cd81ef9
John Wallace <John.Wallace@noaa.gov>
2018-10-09 13:58:24 -0700

    Update scanIn.R

--- R/scanIn.R
+++ R/scanIn.R
@@ -1,9 +1,18 @@
-scanIn <- function(text, header = TRUE, ncol = 2) {
-    Out <- data.frame(matrix(scan(what = "", text = text, quiet = TRUE), ncol = ncol, byrow= TRUE), stringsAsFactors = FALSE)
-    if(header) {
-       names(Out) <- as.character(Out[1,])
-	     Out <- Out[-1,]
-    }
-    JRWToolBox::renum(Out)
-}
-
+scanIn <- function (text, header = TRUE, ncol = 2, matrix = TRUE, numeric = FALSE) 
+{
+    if(matrix) {
+        Out <- data.frame(matrix(scan(what = "", text = text, quiet = TRUE), 
+              ncol = ncol, byrow = TRUE), stringsAsFactors = FALSE)
+        if (header) {
+            names(Out) <- as.character(Out[1, ])
+            Out <- Out[-1, ]
+        }
+        Out <- JRWToolBox::renum(Out)
+     } else {
+        if(numeric)
+            Out <- scan(text = text, quiet = TRUE)
+        else 
+            Out <- scan(what = "", text = text, quiet = TRUE)        
+     }          
+     Out
+}         
