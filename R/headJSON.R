408af2f75f9b8ad7e079949c0591ccfff973ad6a
John Wallace <John.Wallace@noaa.gov>
2018-10-18 10:24:54 -0700

    Changed function name, added verbose option
    and comments.

---
+++ R/headJSON.R
@@ -0,0 +1,17 @@
+headJSON <- function (URL, Vec = 1:5, verbose = FALSE) 
+{
+ '  # Max number of rows downloaded varies by table.  '
+ '  # Strangely, the pagesize argument appears ineffective here.  '
+    JRWToolBox::lib(jsonlite, attach = FALSE)
+    Opt <- options(warn = -1)
+    on.exit(options(Opt))
+    tempObject <- JRWToolBox::randomizeFileName()
+    on.exit(rm(list = tempObject, pos = 1), add = TRUE)
+    jsonlite::stream_in(url(URL), handler = function(df) {
+        for (i in 1) {
+            assign(tempObject, df[Vec, ], pos = 1)
+            break
+        }
+    }, pagesize = 1, verbose = verbose)
+    eval(parse(text = tempObject))
+}
