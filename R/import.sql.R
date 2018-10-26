d0bafd1512b3e96f346c3948b21c0d3c96f14b3b
John Wallace <John.Wallace@noaa.gov>
2018-10-26 13:39:02 -0700

    Added PacFIN example

--- R/import.sql.R
+++ R/import.sql.R
@@ -2,6 +2,8 @@
 import.sql <- function (SQL, VAR = "", VAL = "", File = FALSE, dsn = 'PacFIN', uid, pwd, host = "china.psmfc.org", port = 2045, svc = "pacfin.psmfc.org", 
                         View.Parsed.Only = FALSE, Windows = .Platform$OS.type == "windows") 
 { 
+'  # import.sql( "Select * from pacfin.bds_sp where rownum < 11", dsn = 'PacFIN', host = "china.psmfc.org", port = 2045, svc = "pacfin.psmfc.org", uid='wallacej', pwd= PacFIN.PW )  '
+   
     require(Hmisc)
     if (Windows) 
         require(RODBC)
