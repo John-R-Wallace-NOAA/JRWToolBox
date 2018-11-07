43896b355e2a25e6f504d3337ab5e93a99d30a1a
John Wallace <John.Wallace@noaa.gov>
2018-11-07 10:45:46 -0800

    Added verbose option

--- R/change.R
+++ R/change.R
@@ -1,19 +1,21 @@
-
-change <- function (x) 
+change <- function (x, verbose = TRUE) 
 {
     tf <- ifelse(regexpr("Mydata", search()) == -1, F, T)
     if (any(tf)) {
         pos <- (1:length(search()))[tf]
         detach(pos = pos)
     }
-    if(is.data.frame(x))
-       cat('\nA data frame of size (', nrow(x), ", ", ncol(x), ') has been attached in position 2.\n\n', sep="")
-    else {
-        cat('\nAn object with the following structure has been attached in position 2:\n\n', sep="") 
-        str(x)
-        cat("\n")
+    if(verbose) {
+       if (is.data.frame(x)) 
+           cat("\nA data frame of size (", nrow(x), ", ", ncol(x), 
+               ") has been attached in position 2.\n\n", sep = "")
+       else {
+           cat("\nAn object with the following structure has been attached in position 2:\n\n", 
+               sep = "")
+           str(x)
+           cat("\n")
+       }
     }
     attach(x, 2, paste("Mydata:", substring(deparse(substitute(x))[1], 
         1, 30), sep = ""))
-   
 }
