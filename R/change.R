eca071d9f6d2ba70b9776159df18a525b3504991
John Wallace <John.Wallace@noaa.gov>
2018-11-07 10:48:40 -0800

    test

--- R/change.R
+++ R/change.R
@@ -1,20 +1,19 @@
-change <- function (x, verbose = TRUE) 
+
+change <- function (x) 
 {
     tf <- ifelse(regexpr("Mydata", search()) == -1, F, T)
     if (any(tf)) {
         pos <- (1:length(search()))[tf]
         detach(pos = pos)
     }
-    if(verbose) {
-       if (is.data.frame(x)) 
-           cat("\nA data frame of size (", nrow(x), ", ", ncol(x), 
-               ") has been attached in position 2.\n\n", sep = "")
-       else {
-           cat("\nAn object with the following structure has been attached in position 2:\n\n", 
-               sep = "")
-           str(x)
-           cat("\n")
-       }
+    if (is.data.frame(x)) 
+        cat("\nA data frame of size (", nrow(x), ", ", ncol(x), 
+            ") has been attached in position 2.\n\n", sep = "")
+    else {
+        cat("\nAn object with the following structure has been attached in position 2:\n\n", 
+            sep = "")
+        str(x)
+        cat("\n")
     }
     attach(x, 2, paste("Mydata:", substring(deparse(substitute(x))[1], 
         1, 30), sep = ""))
