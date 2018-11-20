b9d8bd08f6e26061059585a7e157090b8f957ebe
John Wallace <John.Wallace@noaa.gov>
2018-11-20 11:52:35 -0800

    Added a skip number feature

--- R/bar.R
+++ R/bar.R
@@ -1,10 +1,12 @@
-bar <- function (i, n, size = 60, char = ">", OuterIndex = 1, prefix = ifelse(.Platform$OS.type == 
-                 "windows", paste(OuterIndex, ":", memory.size()),  paste(OuterIndex, ":", i))) 
+bar <- function (i, n, size = 60, skipNum = round(n/size), char = ">", OuterIndex = 1, prefix = ifelse(.Platform$OS.type == 
+          "windows", paste(OuterIndex, ":", memory.size()), paste(OuterIndex, ":", i))) 
 {
-    num <- round((size * i)/n)
-    cat(prefix, " |", paste(rep(char, num), collapse = ""), paste(rep(" ", 
-        size - num), collapse = ""), "|\r", sep = "")
-    flush.console()
+    if(i %% skipNum == 0) {
+        num <- round((size * i)/n)
+        cat(prefix, " |", paste(rep(char, num), collapse = ""), paste(rep(" ", 
+            size - num), collapse = ""), "|\r", sep = "")
+        flush.console()
+    }    
     if (i == n) 
         cat("\n\r")
 }
