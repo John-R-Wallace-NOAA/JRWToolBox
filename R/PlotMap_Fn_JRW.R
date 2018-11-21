bb5c615355a2201073af0466e47b670a7a44a218
John Wallace <John.Wallace@noaa.gov>
2018-11-21 14:13:33 -0800

    Included smallPlot() inside this function

--- R/PlotMap_Fn_JRW.R
+++ R/PlotMap_Fn_JRW.R
@@ -7,6 +7,61 @@ PlotMap_Fn_JRW <- function (MappingDetails, Mat, PlotDF, MapSizeRatio = c(`Width
         30)), mfrow = c(1, 1), plot_legend_fig = FALSE, land_color = "grey", 
     ignore.na = FALSE, ...) 
 {
+
+  smallPlot <- function (expr, x = c(5, 70), y = c(50, 100), x1, y1, x2, y2, 
+      mar = c(12, 14, 3, 3), mgp = c(1.8, 0.8, 0), bg = par("bg"), 
+      border = par("fg"), las = 1, resetfocus = TRUE, ...) 
+  {
+      if (missing(x1)) 
+          x1 <- min(x, na.rm = TRUE)
+      if (missing(x2)) 
+          x2 <- max(x, na.rm = TRUE)
+      if (missing(y1)) 
+          y1 <- max(y, na.rm = TRUE)
+      if (missing(y2)) 
+          y2 <- min(y, na.rm = TRUE)
+      if (x1 < 0) {
+          x1 <- 0
+          warning("x (", x1, ") set to 0.")
+      }
+      if (y2 < 0) {
+          y2 <- 0
+          warning("y (", y2, ") set to 0.")
+      }
+      if (x2 > 100) {
+          x2 <- 100
+          warning("x (", x2, ") set to 100.")
+      }
+      if (y1 > 100) {
+          y1 <- 100
+          warning("y (", y1, ") set to 100.")
+      }
+      if (diff(range(x, na.rm = TRUE)) < 1 | diff(range(y, na.rm = TRUE)) < 
+          1) {
+          stop("x or y was probably given as coodinates between 0 and 1. They must be between 0 and 100.")
+      }
+      op <- par(no.readonly = TRUE)
+      par(plt = c(x1, x2, y2, y1)/100, new = TRUE, mgp = mgp)
+      plot.new()
+      u <- par("usr")
+      rect(u[1], u[3], u[2], u[4], col = bg, border = border)
+      par(plt = c(x1 + mar[2], x2 - mar[4], y2 + mar[1], y1 - mar[3])/100, 
+          new = TRUE, las = las, ...)
+      expr
+      sp <- par(no.readonly = TRUE)
+      if (resetfocus) {
+          if (par("mfrow")[1] == 1 & par("mfrow")[2] == 1) {
+              par(op)
+          }
+          else {
+              par(plt = op$plt, new = op$new, mgp = op$mgp, las = op$las)
+          }
+      }
+      return(invisible(sp))
+  }
+
+# ------------------------------------------------------------------
+
     require(maps)
     require(mapdata)
     # on.exit(detach("package:mapdata"))
@@ -109,7 +164,7 @@ PlotMap_Fn_JRW <- function (MappingDetails, Mat, PlotDF, MapSizeRatio = c(`Width
         box()
     }
     if (Legend$use) {
-        SpatialDeltaGLMM:::smallPlot(SpatialDeltaGLMM:::Heatmap_Legend(colvec = Col(50), 
+        smallPlot(SpatialDeltaGLMM:::Heatmap_Legend(colvec = Col(50), 
             heatrange = list(range(Mat[Which, ], na.rm = TRUE), 
                 zlim)[[ifelse(is.null(zlim), 1, 2)]], dopar = FALSE), 
             x = Legend$x, y = Legend$y, mar = c(0, 0, 0, 0), 
