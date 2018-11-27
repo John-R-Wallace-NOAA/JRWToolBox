38c143433b0d9b8a431dba1ff39a2fdf22f07e9b
John Wallace <John.Wallace@noaa.gov>
2018-11-27 15:34:54 -0800

    Fixes to look good with 16 years of data
    at least with LatMin <= 32.25.   Slightly off with longitudeDelta > 2.6

--- R/YearlyResultsFigures.R
+++ R/YearlyResultsFigures.R
@@ -1,5 +1,5 @@
 
-YearlyResultsFigures <- function(spShortName. = NULL, HomeDir = ".", eastLongitude = -160.5, longitudeDelta = 2.6, Index = NULL, SP.Results.Dpth. = NULL, MapDetails_List. = MapDetails_List, Report. = Report, Opt. = Opt, 
+YearlyResultsFigures <- function(spShortName. = NULL, HomeDir = ".", eastLongitude = -124 - (N + 1) * longitudeDelta, longitudeDelta = 2.6, Index. = NULL, SP.Results.Dpth. = NULL, MapDetails_List. = MapDetails_List, Report. = Report, Opt. = Opt, 
                                  DateFile. = DateFile, Year_Set. = Year_Set, Years2Include. = Years2Include, strata.limits. = strata.limits, Ages. = NULL, LenMin. = NULL, LenMax. = NULL, 
                                  yearDelta = 0.5, changeUnitsUnder1Kg = TRUE) {
   
@@ -64,8 +64,9 @@ YearlyResultsFigures <- function(spShortName. = NULL, HomeDir = ".", eastLongitu
     SP.Results$Rescaled.Sum <- SP.Results$Rescaled.Sum - min(SP.Results$Rescaled.Sum)
     SP.Results$Rescaled.Sum <- SP.Results$Rescaled.Sum * 12/max(SP.Results$Rescaled.Sum) + 1
     
-    if(is.null(Index)) 
-        Index <- read.csv(paste0(DateFile., "Table_for_SS3.csv"))[Years2Include., ]
+    # Index <- read.csv(paste0(DateFile., "Table_for_SS3.csv"))[Years2Include., ]
+    if(is.null(Index.) & exists('Index'))
+        Index. <- Index$Table[Years2Include., ]
 
 	if(is.null(spShortName.) & exists('spShortName'))  
         spShortName. <- spShortName
@@ -73,9 +74,16 @@ YearlyResultsFigures <- function(spShortName. = NULL, HomeDir = ".", eastLongitu
          warning("No short species name given nor found.")
 	png(paste0(DateFile., "SpResults ", spShortName., ".png"),  width = 6000, height = 6000, bg = 'white')
 		   
-    par(cex = 6)    
+    par(cex = 6)   
+
+    N <- length(Year_Set.)
+
+    # eastLongitude <- -122 - (N + 1) * longitudeDelta
+    eastLongitude <- eastLongitude 
     
-    Imap::imap(longlat = list(Imap::world.h.land, Imap::world.h.borders), col= c("black", "cyan"), poly = c("grey40", NA), longrange = c(eastLongitude, -117), latrange = c(27, 48.2), zoom= FALSE, bg = "white")  
+    latExtend <- ifelse(N > 13, -((-125 - (N + 1) * 3.5 + 117) - (-125 - 14 * 3.5 + 117))/3, 0)
+       
+    Imap::imap(longlat = list(Imap::world.h.land, Imap::world.h.borders), col= c("black", "cyan"), poly = c("grey40", NA), longrange = c(eastLongitude, -117), latrange = c(27 - latExtend, 48.2), zoom= FALSE, bg = "white")  
     box(lwd = 5)
     
     Col <- colorRampPalette(colors = c("blue", "dodgerblue", "cyan", "green", "orange", "red", "red3"))
@@ -83,24 +91,27 @@ YearlyResultsFigures <- function(spShortName. = NULL, HomeDir = ".", eastLongitu
     COL <- Col(13)[SP.Results$Rescaled.Sum]
     JRWToolBox::hexPolygon(SP.Results$X, SP.Results$Y, hexC = hexcoords(dx = 0.1, sep=NA), col = COL, border = COL)
     
-    N <- length(Year_Set.)
-
     for (i in 1:N) {
        COL <- Col(13)[SP.Results[, N + 3 - i]]
        JRWToolBox::hexPolygon(SP.Results$X - i * longitudeDelta, SP.Results$Y, hexC = hexcoords(dx = 0.1, sep=NA), col = COL, border = COL)
     }
     
-    Index$LongPlotValues <- -124.6437 + seq(-longitudeDelta, by = -longitudeDelta, len = N)
-    Index$LatPlotValues <- rev((48 - 34.2) * (Index$Estimate_metric_tons - min(Index$Estimate_metric_tons))/max(Index$Estimate_metric_tons) + 34.2)
-    Index$LatSD_mt <- rev((48 - 34.2)/(max(Index$Estimate_metric_tons) - min(Index$Estimate_metric_tons)) * Index$SD_mt)
+    Index.$LongPlotValues <- -124.6437 + seq(-longitudeDelta, by = -longitudeDelta, len = N)
+    Index.$LatPlotValues <- rev((48 - 34.2) * (Index.$Estimate_metric_tons - min(Index.$Estimate_metric_tons))/max(Index.$Estimate_metric_tons) + 34.2)
+    Index.$LatSD_mt <- rev((48 - 34.2)/(max(Index.$Estimate_metric_tons) - min(Index.$Estimate_metric_tons)) * Index.$SD_mt)
     
     # It appears that calls to text() need to be before things get changed by using subplot() below.
  
-    GRAMS <- max(exp(SP.Results.Dpth.[,-(1:4)])) < 1 &  changeUnitsUnder1Kg   # Auto change to grams under 1 kg
+    GRAMS <- max(exp(SP.Results.Dpth.[,-(1:4)])) < 1 & changeUnitsUnder1Kg   # Auto change to grams under 1 kg
+    
+    # Converting relative plotting location, 0.5 out of [0, 1] to latitude
+    latAdj <- 0.5 * (48.2 - 27 + latExtend) + 27 - latExtend
+    
+    # text(-118.5, 37.50, ...
     if(GRAMS)
-        text(-118.5, 37.50, 'Grams per Hectare', cex = 0.80)    
+        text(-118.3, latAdj, 'Grams per Hectare', cex = 0.80)    
     else
-        text(-118.5, 37.50, 'Kg per Hectare', cex = 0.85) 
+        text(-118.3, latAdj, 'Kg per Hectare', cex = 0.85) 
 
     LatMin. = strata.limits.$south_border[1]
     
@@ -126,33 +137,35 @@ YearlyResultsFigures <- function(spShortName. = NULL, HomeDir = ".", eastLongitu
     if(LatMin. >= 33.8) {
         text(-120, 33.29, "All", cex = 0.80)
         text(-120, 33.29 - yearDelta, "Years", cex = 0.80)
-        TeachingDemos::subplot( {par(cex = 5); JRWToolBox::plotCI.jrw3(Index$Year, Index$Estimate_metric_tons,  Index$SD_mt, type = 'b', sfrac=0, xlab='Year', ylab = 'Abundance (mt)', 
+        TeachingDemos::subplot( {par(cex = 5); JRWToolBox::plotCI.jrw3(Index.$Year, Index.$Estimate_metric_tons,  Index.$SD_mt, type = 'b', sfrac=0, xlab='Year', ylab = 'Abundance (mt)', 
         col = 'red', lwd = 7, cex =1, xaxt = "n", bty = 'n');  axis(3, Year_Set., lwd = 5); axis(side = 2, lwd = 5)}, 
         x=grconvertX(c(0.08, 0.87), from='npc'), y=grconvertY(c(0.02, 0.28), from='npc'), type='fig', pars=list( mar=c(1.5,4,0,0) + 0.1) )
     
-    } 
+    # } 
 
     if(LatMin. > 32.25 & LatMin. < 33.8) {
         text(-118.0, 32.053, "All", cex = 0.85)
         text(-118.0, 32.053 - yearDelta, "Years", cex = 0.85)
-        TeachingDemos::subplot( {par(cex = 5); JRWToolBox::plotCI.jrw3(Index$Year, Index$Estimate_metric_tons,  Index$SD_mt, type = 'b', sfrac=0, xlab='Year', ylab = 'Abundance (mt)', 
+        TeachingDemos::subplot( {par(cex = 5); JRWToolBox::plotCI.jrw3(Index.$Year, Index.$Estimate_metric_tons,  Index.$SD_mt, type = 'b', sfrac=0, xlab='Year', ylab = 'Abundance (mt)', 
         col = 'red', lwd = 7, cex =1, xaxt = "n", bty = 'n');  axis(3, Year_Set., lwd = 5); axis(side = 2, lwd = 5)}, 
         x=grconvertX(c(0.10, 0.915), from='npc'), y=grconvertY(c(0, 0.225), from='npc'), type='fig', pars=list( mar=c(1.5,4,0,0) + 0.1) )
     }
-
+    
+    # y=grconvertY(c(0, 0.190), from='npc'); x=grconvertX(c(0.10, 0.89), from='npc')
+    xExpand <- ifelse(N > 13, (N - 13) * 0.025/3, 0)
     if(LatMin. <= 32.25) {
         text(-118.7, 31.266, "All", cex = 0.85)
         text(-118.7, 31.266 - yearDelta, "Years", cex = 0.85)
-        TeachingDemos::subplot( {par(cex = 5); JRWToolBox::plotCI.jrw3(Index$Year, Index$Estimate_metric_tons,  Index$SD_mt, type = 'b', sfrac=0, xlab='Year', ylab = 'Abundance (mt)', 
+        TeachingDemos::subplot( {par(cex = 5); JRWToolBox::plotCI.jrw3(Index.$Year, Index.$Estimate_metric_tons,  Index.$SD_mt, type = 'b', sfrac=0, xlab='Year', ylab = 'Abundance (mt)', 
         col = 'red', lwd = 7, cex =1, xaxt = "n", bty = 'n');  axis(3, Year_Set., lwd = 5); axis(side = 2, lwd = 5)}, 
-        x=grconvertX(c(0.10, 0.89), from='npc'), y=grconvertY(c(0, 0.190), from='npc'), type='fig', pars=list( mar=c(1.5,4,0,0) + 0.1) )
+        x=grconvertX(c(0.10 - xExpand, 0.89 + xExpand), from='npc'), y=grconvertY(c(0, (31.03 - 27 + 0.8 * latExtend)/(48.2 - 27 + 0.8 * latExtend)), from='npc'), type='fig', pars=list( mar=c(1.5,4,0,0) + 0.1) )
     }
     
     if(GRAMS)
         TeachingDemos::subplot( { par(cex = 5); color.bar(Col(100), JRWToolBox::r(1000 * min(exp(SP.Results.Dpth.[,-(1:4)])), 0), JRWToolBox::r(1000 * max(exp(SP.Results.Dpth.[,-(1:4)])), 0), nticks = 6) },
             x=grconvertX(c(0.83, 0.87), from='npc'), y=grconvertY(c(0.5, 0.75), from='npc'), type='fig', pars=list( mar=c(0,0,1,0) + 0.1) )    
     else 
-        TeachingDemos::subplot( { par(cex = 5); color.bar(Col(100), JRWToolBox::r(min(exp(SP.Results.Dpth.[,-(1:4)])), 3), JRWToolBox::r(max(exp(SP.Results.Dpth.[,-(1:4)])), 3), nticks = 6) },
+        TeachingDemos::subplot( { par(cex = 5); color.bar(Col(100), JRWToolBox::r(min(exp(SP.Results.Dpth.[,-(1:4)])), 0), JRWToolBox::r(max(exp(SP.Results.Dpth.[,-(1:4)])), 0), nticks = 6) },
             x=grconvertX(c(0.83, 0.87), from='npc'), y=grconvertY(c(0.5, 0.75), from='npc'), type='fig', pars=list( mar=c(0,0,1,0) + 0.1) )    
         
     dev.off()
