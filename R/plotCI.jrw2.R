dc71487301825e7a76031e332a799f9a6fbda661
John Wallace <John.Wallace@noaa.gov>
2018-12-12 15:46:21 -0800

    Added lwd argument

--- R/plotCI.jrw2.R
+++ R/plotCI.jrw2.R
@@ -1,6 +1,6 @@
-plotCI.jrw2 <- 
-function (x, y = NULL, li = NULL, ui = NULL, uiw = NULL, liw = uiw, ylo = NULL, yhi = NULL, 
-    sfrac = 0.01, ymax = NULL, ylim=NULL, add = FALSE, maxValue = NULL, col = "black", ...) 
+plotCI.jrw2 <- function (x, y = NULL, li = NULL, ui = NULL, uiw = NULL, liw = uiw, 
+    ylo = NULL, yhi = NULL, sfrac = 0.01, ymax = NULL, ylim = NULL, 
+    add = FALSE, maxValue = NULL, col = "black", lwd = lwd, ...) 
 {
     if (is.list(x)) {
         y <- x$y
@@ -13,24 +13,22 @@ function (x, y = NULL, li = NULL, ui = NULL, uiw = NULL, liw = uiw, ylo = NULL, 
         y <- as.numeric(x)
         x <- seq(along = x)
     }
-    if(is.null(ui))        {
-      if(is.null(maxValue))
-           ui <- y + uiw
-      else
-           ui <- ifelse(y + uiw > maxValue, maxValue, y + uiw)
+    if (is.null(ui)) {
+        if (is.null(maxValue)) 
+            ui <- y + uiw
+        else ui <- ifelse(y + uiw > maxValue, maxValue, y + uiw)
     }
-    if(is.null(li))
-         li <- y - liw
-    if(is.null(ylim))
+    if (is.null(li)) 
+        li <- y - liw
+    if (is.null(ylim)) 
         ylim <- range(c(y, ui, li, ylo, yhi, ymax))
     if (!add) 
-        plot(x, y, ylim = ylim, col = col, ...)
+        plot(x, y, ylim = ylim, col = col, lwd = lwd, ...)
     else points(x, y, col = col, ...)
     smidge <- diff(par("usr")[1:2]) * sfrac
-    segments(x, li, x, ui, col = col)
+    segments(x, li, x, ui, col = col, lwd = lwd)
     x2 <- c(x, x)
     ul <- c(li, ui)
-    segments(x2 - smidge, ul, x2 + smidge, ul, col = col)
+    segments(x2 - smidge, ul, x2 + smidge, ul, col = col, lwd = lwd)
     invisible(list(x = x, y = y))
 }
-
