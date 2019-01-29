d8e37f9ab2deb90736317d133e4a590882b569b0
John Wallace <John.Wallace@noaa.gov>
2019-01-28 16:27:19 -0800

    Added utils::  to installed.packages()

--- R/lib.R
+++ R/lib.R
@@ -13,21 +13,21 @@ lib <- function (Package, Package.Name = NULL, attach = TRUE, updateCRAN = FALSE
     if (!(is.character(substitute(Package)))) 
         Package <- deparse(substitute(Package))
     if (grepl("/", Package)) {
-        if (any(installed.packages()[, 1] %in% "remotes")) {
+        if (any(utils::installed.packages()[, 1] %in% "remotes")) {
             if (updateCRAN) 
                 update.packages("remotes", ask = FALSE)
         }
         else install.packages("remotes", quiet = quiet)
-        if (!any(installed.packages()[, 1] %in% "remotes")) 
+        if (!any(utils::installed.packages()[, 1] %in% "remotes")) 
             stop(paste("CRAN remotes package is not installed; an attempt to install failed (check for internet access)"))
         if (!(is.character(substitute(Package.Name)))) 
             Package.Name <- deparse(substitute(Package.Name))
         if (Package.Name == "NULL") 
             Package.Name <- get.subs(Package, "/")[[length(get.subs(Package, "/"))]]
-        if (any(installed.packages()[, 1] %in% Package.Name))  
+        if (any(utils::installed.packages()[, 1] %in% Package.Name))  
              SHA.OLD <- packageDescription(Package.Name)$RemoteSha         
         remotes::install_github(Package, quiet = quiet, force = force)
-        if (!any(installed.packages()[, 1] %in% Package.Name)) 
+        if (!any(utils::installed.packages()[, 1] %in% Package.Name)) 
             stop(paste0("R '", Package.Name, "' package from Github is not installed. Note that the R package name may not be the same \n            as the GitHub directory name, if so, use the Package.Name argument. Find the R package name using quiet = FALSE."))
         if (packageDescription(Package.Name)$RemoteSha == SHA.OLD %in% !force)
             stop(paste0("R '", Package.Name, "' package's SHA number did not change"))        
@@ -37,12 +37,12 @@ lib <- function (Package, Package.Name = NULL, attach = TRUE, updateCRAN = FALSE
         }    
     }
     else {
-        if (any(installed.packages()[, 1] %in% Package)) {
+        if (any(utils::installed.packages()[, 1] %in% Package)) {
             if (updateCRAN) 
                 update.packages(Package, ask = FALSE)
         }
         else install.packages(Package, quiet = quiet, ...)
-        if (!any(installed.packages()[, 1] %in% Package)) 
+        if (!any(utils::installed.packages()[, 1] %in% Package)) 
             stop(paste("CRAN", Package, "package is not installed; an attempt to install failed (check for internet access)"))
         if (attach) 
             library(Package, pos = pos, character.only = TRUE)
