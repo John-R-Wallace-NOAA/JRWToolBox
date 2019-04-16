f3449e2bd5f361d332bbcbfd7deb4fc8153f3fb9
John Wallace <John.Wallace@noaa.gov>
2019-04-16 13:54:27 -0700

    Delete browseGitPDF.R

--- R/browseGitPDF.R
+++
@@ -1,6 +0,0 @@
-browseGitPDF <- function(URL) {
-    # https://webapps.stackexchange.com/questions/48061/can-i-trick-github-into-displaying-the-pdf-in-the-browser-instead-of-downloading
-    cat("\nThe pdf is not saved locally until done so manually.\n\n")
-    browseURL(paste0("http://nbviewer.jupyter.org/github/", paste0(strsplit(URL, "/")[[1]][-(1:3)], collapse="/")))    
-}
-
