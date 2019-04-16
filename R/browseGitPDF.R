ac8bb526a3431028204c4f276e21db5106914379
John Wallace <John.Wallace@noaa.gov>
2019-04-16 14:26:44 -0700

    invisible() return the new URL

--- R/browseGitPDF.R
+++ R/browseGitPDF.R
@@ -1,6 +1,7 @@
 browseGitPDF <- function(URL) {
     # https://webapps.stackexchange.com/questions/48061/can-i-trick-github-into-displaying-the-pdf-in-the-browser-instead-of-downloading
     cat("\nThe pdf is not saved locally until done so manually.\n\n")
-    browseURL(paste0("http://nbviewer.jupyter.org/github/", paste0(strsplit(URL, "/")[[1]][-(1:3)], collapse="/")))    
+    newURL <- paste0("http://nbviewer.jupyter.org/github/", paste0(strsplit(URL, "/")[[1]][-(1:3)], collapse="/"))
+    browseURL(newURL)
+    invisible(newURL)    
 }
-
