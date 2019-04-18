414e2793eb9e9e0475b761693d25359407062d3a
John Wallace <John.Wallace@noaa.gov>
2019-04-17 17:55:21 -0700

    Unquoted coomment

--- R/browseGitPDF.R
+++ R/browseGitPDF.R
@@ -1,14 +1,14 @@
 
 
 browseGitPDF <- function(URL) {
-    ' # https://webapps.stackexchange.com/questions/48061/can-i-trick-github-into-displaying-the-pdf-in-the-browser-instead-of-downloading '
+    # https://webapps.stackexchange.com/questions/48061/can-i-trick-github-into-displaying-the-pdf-in-the-browser-instead-of-downloading
     cat("\nThe pdf is not saved locally until done so manually.\n\n")
     newURL <- paste0("http://nbviewer.jupyter.org/github/", paste0(strsplit(URL, "/")[[1]][-(1:3)], collapse="/"))
     browseURL(newURL)
     invisible(newURL)    
 }
 
-if(F)  {
+if(FALSE)  {
 
    # browseURL("https://github.com/dhimmel/delays/blob/icss/ICSS-2016/ICSS-2016-abstract.pdf")
    # browseGitPDF("https://github.com/dhimmel/delays/blob/icss/ICSS-2016/ICSS-2016-abstract.pdf")
