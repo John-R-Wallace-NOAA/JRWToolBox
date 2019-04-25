2dbe8a61d0b4b396180056cb84daa867617e04cc
John Wallace <John.Wallace@noaa.gov>
2019-04-25 14:30:14 -0700

    Added a CDN prefix for GitHub functions

--- R/gitAFile.R
+++ R/gitAFile.R
@@ -1,9 +1,13 @@
-gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGitHub")[1], run = FALSE, show = !run, File = NULL, delete.R.Object = ifelse(type %in% 'function', TRUE, FALSE)) 
+gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGitHub")[1], run = FALSE, show = !run, File = NULL, delete.R.Object = ifelse(is.null(File), TRUE, FALSE), CDN = 'https://cdn.jsdelivr.net/gh/') 
 {
   
-  # Use the CDN jsDelivr name, i.e.: "https://cdn.jsdelivr.net/gh/John-R-Wallace/JRWToolBox@master/R/panel.conf.pred.band.R" 
+  # Adds a CDN jsDelivr prefix to create full URL when type = "function", i.e.: paste0("https://cdn.jsdelivr.net/gh/", "John-R-Wallace/JRWToolBox@master/R/panel.conf.pred.band.R") 
+  # Set CDN = NULL to not add a prefix.
   # CDN jsDelivr homepage:  https://www.jsdelivr.com/
   # csv file download information was from here: http://www.r-bloggers.com/data-on-github-the-easy-way-to-make-your-data-available/ 
+  
+  # Example of pdf download: gitAFile("https://github.com/James-Thorson/VAST/blob/master/manual/VAST_model_structure.pdf", "pdf")
+  # Or directly using browseGitPDF: JRWToolBox::browseGitPDF("https://github.com/James-Thorson/VAST/blob/master/manual/VAST_model_structure.pdf")
   # Displaying pdf's from GitHub: https://webapps.stackexchange.com/questions/48061/can-i-trick-github-into-displaying-the-pdf-in-the-browser-instead-of-downloading 
   
     require(RCurl)
@@ -19,10 +23,13 @@ gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGi
     }
     # ------------------------------------
   
-    if (grepl(type, "csv")) 
+    if(grepl(type, "csv")) 
         return(read.csv(textConnection(getURL(URL))))
         
-    if (grepl(type, "function") | grepl(type, "script") ) {
+    if(grepl(type, "function")) 
+        URL <- paste0(CDN, URL)
+       
+    if(grepl(type, "function") | grepl(type, "script") ) {
         if(is.null(File))
            File.ASCII <- tempfile()
         else 
@@ -34,6 +41,7 @@ gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGi
      
      if(grepl(type, "function")) {
            s.name <- Source(File.ASCII)
+           print(s.name)
            if(run) 
              eval(parse(text = s.name))()
            if(show)
@@ -49,8 +57,8 @@ gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGi
        }
        
        if(grepl(type, "RData")) {
-         ' # https://stackoverflow.com/questions/18833031/download-rdata-and-csv-files-from-ftp-using-rcurl-or-any-other-method '
-         ' # test <- load(rawConnection(getBinaryURL(URL)))  # Does not work for me on binary RData files '
+          # https://stackoverflow.com/questions/18833031/download-rdata-and-csv-files-from-ftp-using-rcurl-or-any-other-method 
+          # test <- load(rawConnection(getBinaryURL(URL)))  # Does not work for me on binary RData files 
          if(is.null(File))
             File.BINARY <- tempfile()
          else 
