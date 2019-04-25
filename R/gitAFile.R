bc38c668fd7dd90f1dc0adeedc08c6cd9c52e886
John Wallace <John.Wallace@noaa.gov>
2019-04-25 16:10:50 -0700

    Added purge to CDN
    and baseenv() to base::source() in Source()

--- R/gitAFile.R
+++ R/gitAFile.R
@@ -1,10 +1,11 @@
-gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGitHub")[1], run = FALSE, show = !run, File = NULL, delete.R.Object = ifelse(is.null(File), TRUE, FALSE), CDN = 'https://cdn.jsdelivr.net/gh/') 
+gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGitHub")[1], run = FALSE, show = !run, File = NULL, delete.R.Object = ifelse(is.null(File), TRUE, FALSE), CDN.prefix = TRUE, CDN.purge = TRUE) 
 {
   
   # Adds a CDN jsDelivr prefix to create full URL when type = "function", i.e.: paste0("https://cdn.jsdelivr.net/gh/", "John-R-Wallace/JRWToolBox@master/R/panel.conf.pred.band.R") 
   # Set CDN = NULL to not add a prefix.
   # CDN jsDelivr homepage:  https://www.jsdelivr.com/
   # csv file download information was from here: http://www.r-bloggers.com/data-on-github-the-easy-way-to-make-your-data-available/ 
+  # Purging CDN: https://github.com/video-dev/hls.js/issues/1664
   
   # Example of pdf download: gitAFile("https://github.com/James-Thorson/VAST/blob/master/manual/VAST_model_structure.pdf", "pdf")
   # Or directly using browseGitPDF: JRWToolBox::browseGitPDF("https://github.com/James-Thorson/VAST/blob/master/manual/VAST_model_structure.pdf")
@@ -18,7 +19,7 @@ gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGi
                 base::ls()
             })
         }
-        base::source(file, ...)
+        base::source(file, baseenv(), ...)
         ls.ext(file)
     }
     # ------------------------------------
@@ -26,8 +27,12 @@ gitAFile <- function (URL, type = c("function", "csv", "script", "RData", "pdfGi
     if(grepl(type, "csv")) 
         return(read.csv(textConnection(getURL(URL))))
         
-    if(grepl(type, "function")) 
-        URL <- paste0(CDN, URL)
+    if(grepl(type, "function") & ) {
+        if(CDN.prefix) 
+           URL <- paste0('https://cdn.jsdelivr.net/gh/', URL)
+        if((CDN.purge)    
+           getURL(paste0('https://purge.jsdelivr.net/gh/', URL))
+    }    
        
     if(grepl(type, "function") | grepl(type, "script") ) {
         if(is.null(File))
