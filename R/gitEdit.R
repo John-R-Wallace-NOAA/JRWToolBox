65c8152f5dd1ce594411da25f3c14dd80a5d54b6
John Wallace <John.Wallace@noaa.gov>
2019-04-29 11:43:08 -0700

    Added gitPath arg

--- R/gitEdit.R
+++ R/gitEdit.R
@@ -1,7 +1,7 @@
-gitEdit <- function(File) {
+itEdit <- function(File, gitPath = "John-R-Wallace-NOAA/JRWToolBox/master/R/") {
     if (!(is.character(substitute(File)))) 
        File <- paste0(deparse(substitute(File)), ".R")
-    JRWToolBox::gitAFile(paste0("John-R-Wallace-NOAA/JRWToolBox/master/R/", File), 'script', File = File, show = FALSE)
+    JRWToolBox::gitAFile(paste0(gitPath, File), 'script', File = File, show = FALSE)
     system(paste0("C:/Windows/System32/cmd.exe /C start W:/Win_apps/npp/notepad++.exe ", getwd(), "/", File))
 }
 
