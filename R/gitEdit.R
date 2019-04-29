3b03ca21b19c5437588c5179fe0a2dd31f5a0e08
John Wallace <John.Wallace@noaa.gov>
2019-04-29 11:32:53 -0700

    New

---
+++ R/gitEdit.R
@@ -0,0 +1,7 @@
+gitEdit <- function(File) {
+    if (!(is.character(substitute(File)))) 
+       File <- paste0(deparse(substitute(File)), ".R")
+    JRWToolBox::gitAFile(paste0("John-R-Wallace-NOAA/JRWToolBox/master/R/", File), 'script', File = File, show = FALSE)
+    system(paste0("C:/Windows/System32/cmd.exe /C start W:/Win_apps/npp/notepad++.exe ", getwd(), "/", File))
+}
+
