e5203ba9607b2e9018d48cd66f759ccf78f0e4e6
John Wallace <John.Wallace@noaa.gov>
2019-04-29 20:05:27 -0700

    Added Notepadd++ on C drive

--- R/gitEdit.R
+++ R/gitEdit.R
@@ -1,7 +1,12 @@
-gitEdit <- function(File, gitPath = "John-R-Wallace-NOAA/JRWToolBox/master/R/") {
+gitEdit <- function (File, gitPath = "John-R-Wallace-NOAA/JRWToolBox/master/R/") 
+{
     if (!(is.character(substitute(File)))) 
-       File <- paste0(deparse(substitute(File)), ".R")
-    JRWToolBox::gitAFile(paste0(gitPath, File), 'script', File = File, show = FALSE)
-    system(paste0("C:/Windows/System32/cmd.exe /C start W:/Win_apps/npp/notepad++.exe ", getwd(), "/", File))
+        File <- paste0(deparse(substitute(File)), ".R")
+    JRWToolBox::gitAFile(paste0(gitPath, File), "script", 
+        File = File, show = FALSE)
+    if(file.exists('W:/Win_apps/npp/notepad++.exe'))
+       system(paste0("C:/Windows/System32/cmd.exe /C start W:/Win_apps/npp/notepad++.exe ", getwd(), "/", File))
+    if(file.exists('C:/Win_apps/Notepad++/notepad++.exe'))
+       system(paste0("C:/Windows/System32/cmd.exe /C start C:/Win_apps/Notepad++/notepad++.exe ",getwd(), "/", File))
+    invisible()
 }
-
