2877b34bdd19d33671b686c69a3a7e874dfc6afc
John Wallace <John.Wallace@noaa.gov>
2019-05-01 14:10:41 -0700

    Fix quotes for a path with spaces

--- R/gitEdit.R
+++ R/gitEdit.R
@@ -5,8 +5,9 @@ gitEdit <- function (File, gitPath = "John-R-Wallace-NOAA/JRWToolBox/master/R/")
     JRWToolBox::gitAFile(paste0(gitPath, File), "script", 
         File = File, show = FALSE)
     if(file.exists('W:/Win_apps/npp/notepad++.exe'))
-       system(paste0("C:/Windows/System32/cmd.exe /C start W:/Win_apps/npp/notepad++.exe ", getwd(), "/", File))
+       system(paste0('C:/Windows/System32/cmd.exe /C start W:/Win_apps/npp/notepad++.exe "', getwd(), '/', File, '"'))
     if(file.exists('C:/Win_apps/Notepad++/notepad++.exe'))
-       system(paste0("C:/Windows/System32/cmd.exe /C start C:/Win_apps/Notepad++/notepad++.exe ",getwd(), "/", File))
+       system(paste0('C:/Windows/System32/cmd.exe /C start C:/Win_apps/Notepad++/notepad++.exe "', getwd(), '/', File, '"'))
     invisible()
 }
+
