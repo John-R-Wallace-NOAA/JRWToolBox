bd025cecefa57c7c62d315745cf552d81196a30d
John Wallace <John.Wallace@noaa.gov>
2019-05-16 17:48:23 -0700

    added 'show' arg

--- R/S.R
+++ R/S.R
@@ -1,9 +1,9 @@
 
-S <- function(File, ...) {
+S <- function(File, show = TRUE, ...) {
     if (!(is.character(substitute(File))))
         File <- deparse(substitute(File))
 
-     gitAFile(paste0("John-R-Wallace-NOAA/JRWToolBox/master/R/", File, ".R"), ...)     
+     gitAFile(paste0("John-R-Wallace-NOAA/JRWToolBox/master/R/", File, ".R"), show = show, ...)     
 }
 
 
