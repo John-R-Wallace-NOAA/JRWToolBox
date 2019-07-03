7d4e4796b86b8fdc6f941c7013cc9db509e9109b
John Wallace <John.Wallace@noaa.gov>
2019-07-03 16:29:05 -0700

    Create Run.R

---
+++ R/Run.R
@@ -0,0 +1,13 @@
+Run <- function (File, run = TRUE, viewOnly = TRUE, ...) 
+{
+    if (!(is.character(substitute(File)))) 
+        File <- deparse(substitute(File))
+    gitAFile(paste0("John-R-Wallace-NOAA/JRWToolBox/master/R/", 
+        File, ".R"), run = run , viewOnly = viewOnly, ...)
+}
+
+# Run(Ls, pat='bds')
+
+
+
+
