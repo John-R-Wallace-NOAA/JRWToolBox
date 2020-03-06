45cd72204dbe4e95e2baca8d3e1ce7806980304a
John Wallace <John.Wallace@noaa.gov>
2020-03-05 16:27:44 -0800

    Added arg warnPackageUpdateOnly

--- R/lib.R
+++ R/lib.R
@@ -1,5 +1,5 @@
 lib <- function (Package, Package.Name = NULL, attach = TRUE, updateCRAN = FALSE, 
-    pos = 2, quiet = FALSE, force = FALSE, autoAddRepo = TRUE, ...) 
+    pos = 2, quiet = FALSE, warnPackageUpdateOnly = quiet, force = FALSE, autoAddRepo = TRUE, ...) 
 {
    oldOpts <- options(download.file.method = "auto")  # Sometimes remotes::install_github() throws an error without this
    on.exit(options(oldOpts))
@@ -14,29 +14,41 @@ lib <- function (Package, Package.Name = NULL, attach = TRUE, updateCRAN = FALSE
     "  "
     if (!(is.character(substitute(Package)))) 
         Package <- deparse(substitute(Package))
+        
     if (grepl("/", Package)) {
         if (any(utils::installed.packages()[, 1] %in% "remotes")) {
             if (updateCRAN) 
                 update.packages("remotes", ask = FALSE)
         }
         else install.packages("remotes", quiet = quiet)
+        
         if (!any(utils::installed.packages()[, 1] %in% "remotes")) 
             stop(paste("CRAN remotes package is not installed; an attempt to install failed (check for internet access)"))
+            
         if (!(is.character(substitute(Package.Name)))) 
             Package.Name <- deparse(substitute(Package.Name))
+            
         if (Package.Name == "NULL") 
             Package.Name <- get.subs(Package, "/")[[length(get.subs(Package, "/"))]]
+            
 		if(!quiet) 
 	        on.exit(cat("\nPackage = ", Package, "; Package.Name = ", Package.Name, "\n\n", sep = ""), add = TRUE)
+            
         if (any(utils::installed.packages()[, 1] %in% Package.Name))  
-             SHA.OLD <- packageDescription(Package.Name)$RemoteSha         
+             SHA.OLD <- packageDescription(Package.Name)$RemoteSha 
+             
         remotes::install_github(Package, quiet = quiet, force = force, ...)
+        
         if (!any(utils::installed.packages()[, 1] %in% Package.Name)) 
             stop(paste0("R '", Package.Name, "' package from Github is not installed. Note that the R package name may not be the same \n            as the GitHub directory name, if so, use the Package.Name argument. Find the R package name using quiet = FALSE."))
+        
         if(exists('SHA.OLD') && length(SHA.OLD) > 0) {
-          if (packageDescription(Package.Name)$RemoteSha == SHA.OLD)
-              warning(paste0("R '", Package.Name, "' package's SHA number did not change"))   
-        }     
+          if (packageDescription(Package.Name)$RemoteSha == SHA.OLD & !warnPackageUpdateOnly & !quiet)
+              warning(paste0("R '", Package.Name, "' package's SHA number did not change"))
+          if (packageDescription(Package.Name)$RemoteSha != SHA.OLD & warnPackageUpdateOnly)
+              warning(paste0("R '", Package.Name, "' package was updated to the latest version"))                 
+        }
+        
         if (attach) {
             unloadNamespace(Package.Name)   
             library(Package.Name, pos = pos, character.only = TRUE)
@@ -47,11 +59,14 @@ lib <- function (Package, Package.Name = NULL, attach = TRUE, updateCRAN = FALSE
                 update.packages(Package, ask = FALSE)
         }
         else install.packages(Package, quiet = quiet, ...)
+        
         if (!any(utils::installed.packages()[, 1] %in% Package)) 
             stop(paste("CRAN", Package, "package is not installed; an attempt to install failed (check for internet access)"))
+            
         if (attach) 
             library(Package, pos = pos, character.only = TRUE)
     }
 	
 }
+
 
