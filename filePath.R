658844b18e01eccd91b64b1fb618638ff8156ebf
John Wallace <John.Wallace@noaa.gov>
2020-04-16 17:07:07 -0700

    Delete filePath.R

--- filePath.R
+++
@@ -1,9 +0,0 @@
- 
-filePath <- function(oldPath = readClipboard()) 
-{
-    '  # filePath takes path from the clipboard and replaces it with the machine dependent correct path, which is also output  '
-    correctPath <- do.call(file.path, as.list(strsplit(gsub("\\\\", " ", oldPath), " ")[[1]]))
-    cat(correctPath, file = "clipboard")
-    correctPath
-}
-
