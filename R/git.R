46b0c6fca637f2681b5f3b8d308a80915a96d03d
John Wallace <John.Wallace@noaa.gov>
2020-05-14 02:18:34 -0700

    Needed /wait arg !!!!

--- R/git.R
+++ R/git.R
@@ -1,11 +1,7 @@
-git <- function(gitCommand, sleepSecs = 2) {
-    shell(paste0("echo git ", gitCommand,  " > run.bat"))
+git <- function (gitCommand) 
+{
+    shell(paste0("echo git ", gitCommand, " > run.bat"))
     shell("echo exit >> run.bat")
-    shell("start run.bat")
-    '  # Default is for shell() to wait until process is done, but this pause appears needed  '
-    Sys.sleep(sleepSecs)  
+    shell("start /wait run.bat")
     shell("del run.bat")
-    Sys.sleep(2)
-}
-
-
+}   
