d3ab36b05899aa37b0728198cc30b7ea005d192c
John Wallace <John.Wallace@noaa.gov>
2020-05-14 01:46:44 -0700

    Update git.f

--- R/git.f
+++ R/git.f
@@ -1,9 +1,9 @@
 git <- function(gitCommand, sleepSecs = 2) {
-
     shell(paste0("echo git ", gitCommand,  " > run.bat"))
     shell("echo exit >> run.bat")
     shell("start run.bat")
-    Sys.sleep(sleepSecs)  # Default is for shell() to wait until process is done, but this pause appears needed
+    '  # Default is for shell() to wait until process is done, but this pause appears needed  '
+    Sys.sleep(sleepSecs)  
     shell("del run.bat")
     Sys.sleep(2)
 }
