1af786dc385198ab1954c06e852e7acc7f7a7267
John Wallace <John.Wallace@noaa.gov>
2020-06-15 16:15:30 -0700

    Delete git.sf1.R

--- R/git.sf1.R
+++
@@ -1,3 +0,0 @@
-    g1 <- function(run = TRUE, show = !run, type = 'script') {
- gitAFile("https://raw.githubusercontent.com/John-R-Wallace/Scratch/master/sf1.R", run = run, show = show, type = type)
-}
