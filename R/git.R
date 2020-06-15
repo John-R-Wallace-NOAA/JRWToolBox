95fb90c45197e57bdbe43de8205a7c9a596e6b1e
John Wallace <John.Wallace@noaa.gov>
2020-06-15 16:15:16 -0700

    Delete git.R

--- R/git.R
+++
@@ -1,7 +0,0 @@
-git <- function (gitCommand) 
-{
-    shell(paste0("echo git ", gitCommand, " > run.bat"))
-    shell("echo exit >> run.bat")
-    shell("start /wait run.bat")
-    shell("del run.bat")
-}   
