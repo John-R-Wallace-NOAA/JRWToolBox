416c090c7bfca6175c6e63a85994975289e16408
John Wallace <John.Wallace@noaa.gov>
2020-06-15 16:40:29 -0700

    Delete S.R

--- R/S.R
+++
@@ -1,9 +0,0 @@
-
-S <- function(File, show = TRUE, ...) {
-    if (!(is.character(substitute(File))))
-        File <- deparse(substitute(File))
-
-     gitAFile(paste0("John-R-Wallace-NOAA/JRWToolBox/master/R/", File, ".R"), show = show, ...)     
-}
-
-
