011725a7b171b0c6affed4176bd987c0a657cc3d
John Wallace <John.Wallace@noaa.gov>
2020-06-26 12:27:36 -0700

    Changed with rgit

--- R/convert.hr.min.sec.to.decimal.hrs.R
+++ R/convert.hr.min.sec.to.decimal.hrs.R
@@ -1,8 +1,21 @@
-convert.hr.min.sec.to.decimal.hrs <- function(hr.min.sec)
-{
-	if(hr.min.sec %in% "<NA>")
-		return(NA)
-	tmp <- as.numeric(get.subs(hr.min.sec, sep = ":"))
-	tmp[1] + tmp[2]/60 + tmp[3]/3600
-}
+
+convert.hr.min.sec.to.decimal.hrs <- function(hr.min.sec, sep = ':') {
+
+    convert <- function(hr.min.sec, sep = ':')
+    {
+            if(hr.min.sec %in% "<NA>" | is.na(hr.min.sec))
+                    return(NA)
+    	    if(length(get.subs(hr.min.sec, sep = sep)) == 1 )
+    		     tmp <- as.numeric(c(substr(hr.min.sec, 1, 2), substr(hr.min.sec, 3, 4), substr(hr.min.sec, 5, 6)))
+    	    else		 
+                 tmp <- as.numeric(get.subs(hr.min.sec, sep = sep))
+    			 
+            tmp[1] + tmp[2]/60 + tmp[3]/3600
+    }
+    
+
+   apply(matrix(hr.min.sec, ncol = 1), 1, convert, sep = sep)
+  
+ } 
+
 
--- R/renum.R
+++ R/renum.R
@@ -1,9 +1,10 @@
-renum <-
-function(x, no.num = F)
+renum <- function (x, no.num = F) 
 {
-	if(no.num)
-		dimnames(x)[[1]] <- rep("", nrow(x))
-	else dimnames(x)[[1]] <- 1:nrow(x)
-	x
+    if(nrow(x) == 0)
+         return(x)    
+    if (no.num) 
+        dimnames(x)[[1]] <- rep("", nrow(x))
+    else dimnames(x)[[1]] <- 1:nrow(x)
+    x
 }
 
