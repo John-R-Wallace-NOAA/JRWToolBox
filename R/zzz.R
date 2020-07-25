23819777739b07b735bd068247ca3bdec4881389
John Wallace <John.Wallace@noaa.gov>
2020-07-24 20:12:10 -0700

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,22 @@
+ 
+.onAttach <- function(libname, pkgname) {
+
+    packageStartupMessage("###########################################################################################")
+    packageStartupMessage("Loading JRWToolbox and rgit packages - Welcome")
+    packageStartupMessage("###########################################################################################")
+    
+    if( getOption("repos")["CRAN"] == "@CRAN@" ) {
+    
+        options(repos=c(CRAN="https://cloud.r-project.org/", CRANextra = "http://lib.stat.cmu.edu/R/CRAN/"))
+    }
+    
+    if( !"rgit" %in% utils::installed.packages()[,1] ) {
+    
+      devtools::install_github("John-R-Wallace-NOAA/rgit")
+  	  library(rgit)
+  	  
+    } else {
+  	
+  	  library(rgit)
+    }
+}
