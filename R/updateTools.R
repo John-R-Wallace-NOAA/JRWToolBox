6087423b821561b8f61f396ff09d9a5c7d416e9f
John Wallace <John.Wallace@noaa.gov>
2020-09-14 19:20:11 -0700

    INSTALL_opts = "--no-staged-install"

--- R/updateTools.R
+++ R/updateTools.R
@@ -1,5 +1,5 @@
 
-updateTools <- function(quiet = FALSE, ...) {
+updateTools <- function(quiet = FALSE, INSTALL_opts = "--no-staged-install", ...) {
 
    if(any(grepl('rgit', as.data.frame(installed.packages())$Package)))
       rgit::update_rgit()
@@ -7,6 +7,6 @@ updateTools <- function(quiet = FALSE, ...) {
    if(any(search() %in% "package:JRWToolBox"))
       detach("package:JRWToolBox")
    
-   JRWToolBox::lib("John-R-Wallace-NOAA/JRWToolBox", quiet = quiet, ...)
+   JRWToolBox::lib("John-R-Wallace-NOAA/JRWToolBox", quiet = quiet, INSTALL_opts = INSTALL_opts, ...)
 }
 
