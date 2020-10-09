2e01a0172c480e0f6ab22b6159158b1c4047b191
John Wallace <John.Wallace@noaa.gov>
2020-10-09 12:19:43 -0700

    Update ino.R

--- R/ino.R
+++ R/ino.R
@@ -1,3 +1,11 @@
-asdfasdf
+"%ino%" <- function (x, table)  {
+    "  # %ino% keeps the order of elements listed in the table argument, %in% gives the order found in x  "
+    "  # https://stackoverflow.com/questions/10586652/r-preserve-order-when-using-matching-operators-in  "
+    "  # LETTERS[LETTERS %in% c('Z', 'C' 'A')]  => [1] 'A' 'C' 'Z'  "
+    "  # LETTERS[LETTERS %ino% c('Z', 'C', 'A')]  => [1] 'Z' 'C' 'A' "
 
-df
+    xSeq <- seq(along = x)
+    names(xSeq) <- x
+    Out <- xSeq[as.character(table)]
+    Out[!is.na(Out)]
+  }
