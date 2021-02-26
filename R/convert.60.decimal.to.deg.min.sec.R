e36e45b886f86f6e32acd5543ff454b48ed5a0e8
John Wallace <John.Wallace@noaa.gov>
2021-02-26 13:02:44 -0800

    Vectorized it

--- R/convert.60.decimal.to.deg.min.sec.R
+++ R/convert.60.decimal.to.deg.min.sec.R
@@ -1,11 +1,14 @@
 
 
 convert.60.decimal.to.deg.min.sec <- function(x) {
- 
-   x2 <- floor(x)
-   x3 <- (x - x2) * 60
-   x4 <- floor(x3)
-   x5 <- (x3 - x4) * 60
-   x6 <- round(x5 <- (x3 - x4) * 60)
-   paste(x2, x4, x6, sep = ":")
+    ' # Degree could be hours also, of course. '
+    convert.60.dec <- function(x) {
+       x2 <- floor(x)
+       x3 <- (x - x2) * 60
+       x4 <- floor(x3)
+       x5 <- (x3 - x4) * 60
+       x6 <- round(x5 <- (x3 - x4) * 60)
+       paste(x2, x4, x6, sep = ":")
+    }  
+    apply(matrix(x, ncol = 1), 1, convert.60.dec)
 }
