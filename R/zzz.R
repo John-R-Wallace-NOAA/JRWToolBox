bb0b7d1d9b7c33510e1a3d0b718c76656f277b7b
John Wallace <John.Wallace@noaa.gov>
2021-05-04 16:37:00 -0700

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
  
-.onAttach<- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
 
     packageStartupMessage("#######################################################")
     # packageStartupMessage("Loading JRWToolbox and rgit packages - Welcome")
