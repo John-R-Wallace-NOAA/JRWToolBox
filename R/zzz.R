7bd2804f1f5c3c889d7dabffb7f0c3e160db9d01
John Wallace <John.Wallace@noaa.gov>
2021-05-06 18:41:14 -0700

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -42,21 +42,21 @@
     packageStartupMessage("#######################################################")
     
     
-    if( getOption("repos")["CRAN"] == "@CRAN@" ) {
-    
-        options(repos=c(CRAN="https://cloud.r-project.org/", CRANextra = "http://lib.stat.cmu.edu/R/CRAN/"))
-    }
-    
-    if( !"rgit" %in% utils::installed.packages()[,1] ) {
-    
-        devtools::install_github("John-R-Wallace-NOAA/rgit")
-        
-    } else {
-        
-       if('package:rgit' %in% search())
-          detach("package:rgit")
-   }
-   library(rgit, pos = 3)  
+    # if( getOption("repos")["CRAN"] == "@CRAN@" ) {
+    # 
+    #     options(repos=c(CRAN="https://cloud.r-project.org/", CRANextra = "http://lib.stat.cmu.edu/R/CRAN/"))
+    # }
+    # 
+    # if( !"rgit" %in% utils::installed.packages()[,1] ) {
+    # 
+    #     devtools::install_github("John-R-Wallace-NOAA/rgit")
+    #     
+    # } else {
+    #     
+    #    if('package:rgit' %in% search())
+    #       detach("package:rgit")
+    # }
+    # library(rgit, pos = 3)  
 }
 
 
