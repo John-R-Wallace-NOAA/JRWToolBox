1aba47d35a4e3d64d0a74ba8fabf7a1ef7b4c188
John Wallace <John.Wallace@noaa.gov>
2021-06-30 13:48:31 -0700

    Changed with rgit

--- R/lsp.R
+++ R/lsp.R
@@ -1,7 +1,15 @@
 lsp <- function(package, what, pattern) {
-  # https://stackoverflow.com/questions/12114355/show-names-of-everything-in-a-package 
+
+  # Adapted from: https://stackoverflow.com/questions/12114355/show-names-of-everything-in-a-package
+  
   if (!is.character(substitute(package)))
     package <- deparse(substitute(package))
+    
+  suppressWarnings(package <- ifelse(is.na(as.numeric(package)), package, as.numeric(package)))
+ 
+  if (is.numeric(package))
+    package <- JRWToolBox::get.subs(search()[package], ':')[2]
+   
   ns <- asNamespace(package)
   if (missing(pattern))
     pattern <- '.*'
@@ -9,13 +17,16 @@ lsp <- function(package, what, pattern) {
   ## base package does not have NAMESPACE
   if (isBaseNamespace(ns)) {
     res <- ls(.BaseNamespaceEnv, all.names = TRUE)
-    return(res[grep(pattern, res, perl = TRUE, ignore.case = TRUE)])
+    res_pat <- res[grep(pattern, res, perl = TRUE, ignore.case = TRUE)]
+    print(res_pat, quote = FALSE)
+    return(invisible(res_pat))
   } else {
     ## for non base packages
     if (exists('.__NAMESPACE__.', envir = ns, inherits = FALSE)) {
       wh <- get('.__NAMESPACE__.', inherits = FALSE,
                 envir = asNamespace(package, base.OK = FALSE))
-      what <- if (missing(what)) 'all'
+      if (missing(what)) # Fixed from the Stack Over Flow reference
+         what <- 'all'
       else if ('?' %in% what) return(ls(wh)) 
       else ls(wh)[pmatch(what[1], ls(wh))]
       if (!is.null(what) && !any(what %in% c('all', ls(wh))))
@@ -25,16 +36,24 @@ lsp <- function(package, what, pattern) {
       res <- sapply(ls(wh), function(x) getNamespaceInfo(ns, x))
       res <- rapply(res, ls, classes = 'environment',
                     how = 'replace', all.names = TRUE)
-      if (is.null(what))
-        return(res[grep(pattern, res, perl = TRUE, ignore.case = TRUE)])
+      if (is.null(what)) {
+        res_pat <- res[grep(pattern, res, perl = TRUE, ignore.case = TRUE)]
+        print(res_pat, quote = FALSE)
+        invisible(res_pat)
+      }  
       if (what %in% 'all') {
         res <- ls(getNamespace(package), all.names = TRUE)
-        return(res[grep(pattern, res, perl = TRUE, ignore.case = TRUE)])
+        res_pat <- res[grep(pattern, res, perl = TRUE, ignore.case = TRUE)]
+        print(res_pat, quote = FALSE)
+        return(invisible(res_pat))
       }
       if (any(what %in% ls(wh))) {
         res <- res[[what]]
-        return(res[grep(pattern, res, perl = TRUE, ignore.case = TRUE)])
+        res_pat <- res[grep(pattern, res, perl = TRUE, ignore.case = TRUE)]
+        print(res_pat, quote = FALSE)
+        return(invisible(res_pat))
       }
     } else stop(sprintf('no NAMESPACE file found for package %s', package))
   }
 }
+
--- R/plot.bubble.zero.cross.R
+++ R/plot.bubble.zero.cross.R
@@ -1,7 +1,8 @@
-plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Groups = unique(group), Circle.Area.Prop.to.Z = TRUE, maxsize = scale.size * diff(range(xyzRaw[, 2], na.rm = TRUE)), 
+plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Groups = unique(group), Circle.Area.Prop.to.Z = TRUE, maxsize = scale.size * diff(range(xyzRaw[, 2], na.rm = TRUE)),
     scale.size = 0.07, largestSqrtZ = max(xyzSqrt[, 3], na.rm = TRUE), center.points = FALSE, center.cex = 0.5, xDelta = 0, add = FALSE, 
     xlab = dimnames(xyz)[[2]][1], ylab = dimnames(xyz)[[2]][2], main = NULL, range.bump = FALSE, cross.cex = 1, adj = NULL, 
-    fill.col = c("green", "red", "blue", "cyan", "black"), fill.col.alpha = 0.2,  border.col = "black", border.col.alpha = fill.col.alpha, 
+    fill.col = colorRampPalette(colors = c("dodgerblue", "orange", "cyan", "magenta", "green", "red", "blue", "violetred4", "cyan3", "mediumorchid3", "seagreen4", "black"))(length(Groups)), fill.col.alpha = 0.2, 
+    border.col = "black", border.col.alpha = fill.col.alpha, 
     cross.col = {
         if (is.null(fill.col)) {
             border.col
@@ -9,30 +10,36 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
         else {
             fill.col
         }
-    }, cross.col.alpha = ifelse(fill.col.alpha + 0.65 > 1, 1, fill.col.alpha + 0.5), border.lwd = 1.25, PCH = FALSE, legend = TRUE, 
-    legLoc = c(0.1, 0.25), legCol = "grey4", legAlpha = 0.5, legUnits = "Metric Tons", legNsmall = 1, Extra.Group.Size = rep(1, N), verbose = FALSE, ...)  {
+    }, cross.col.alpha = ifelse(fill.col.alpha + 0.65 > 1, 1, fill.col.alpha + 0.5), border.lwd = 1.25, PCH = FALSE, legend = TRUE, maxZ = NULL,
+    
+    
+    legLoc = c(0.1, 0.25), legCol = "grey4", legAlpha = 0.5, legUnits = "Metric Tons", legNsmall = 1, Extra.Group.Size = rep(1, N), verboseGroup = FALSE, verboseTimeBar = FALSE, ...)  {
+    
+    '  # **** Data is proportional to the area of the circle ****  '
 
-    " # Need to define below in case toolbox is not attached. "
+    '  # Need to define the fucntions below in case the toolbox is not attached. '
     '%>>%' <- function (x, y) {
         x > y & !is.na(x)
     }
 	
-
-    " **** Data is proportional to the area of the circle **** "
+    '  # Being forced to use length(Groups) not N in the fill.col arg because Win R looks for that N outside the function. Linux R looks inside the function for that N.  '
+    
+    '  # Remainder function that gives back the divisor, not zero, when evenly divisible, e.g. 14 %r1% 7 gives 7 not 0  '
     "%r1%" <- function(e1, e2) ifelse(e1%%e2 == 0, e2, e1%%e2)
+    
+    '  # col.alpha() conveniently puts together the color and alpha level into a new color that can be directly used by functions like lines() and polygon(). '
     col.alpha <- function(col, alpha = 0.5) {
         FUNC <- function(col, alpha = alpha) {
             COL <- col2rgb(col)/255
             rgb(red = COL[1], green = COL[2], blue = COL[3], 
                 alpha = alpha)
         }
-        for (i in 1:length(col)) col[i] <- FUNC(col[i], alpha = alpha[i %r1% 
-            length(alpha)])
+        for (i in 1:length(col)) 
+             col[i] <- FUNC(col[i], alpha = alpha[i %r1% length(alpha)])
         col
     }
     
     
-
     if(nrow(xyzRaw) != length(group)) 
        stop("The number of rows of 'xyzRaw' needs to equal the length of the 'group' argument.")
     
@@ -47,15 +54,17 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
        xyzSqrt <- xyzRaw
        xyzSqrt[, 3] <- sign(xyzSqrt[, 3]) * sqrt(abs(xyzSqrt[, 3]))
        xyz <- xyzSqrt
+       xyz[, 3] <- ifelse(is.null(maxZ), maxsize, sqrt(max(xyzRaw[, 3], na.rm = TRUE)/maxZ) * maxsize) * xyz[, 3]/ifelse(largestSqrtZ %in% 0, 1, largestSqrtZ)
     } else {
        xyz <- xyzSqrt <- xyzRaw
+       xyz[, 3] <- ifelse(is.null(maxZ), maxsize, (max(xyzRaw[, 3], na.rm = TRUE)/maxZ) * maxsize) * xyz[, 3]/ifelse(largestSqrtZ %in% 0, 1, largestSqrtZ)
     }
     
-    xyz[, 3] <- (maxsize * xyzSqrt[, 3])/ifelse(largestSqrtZ %in% 0, 1, largestSqrtZ)
     tf <- !apply(xyz, 1, function(x) any(is.na(x)))
     xyz <- xyz[tf, ]
     group <- group[tf]
     N <- length(Groups)
+   
 	if (!add) {
         if (range.bump) {
             xlim <- c(min(xyz[, 1], na.rm = T) - 0.2 * diff(range(xyz[, 
@@ -81,7 +90,7 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
     for (j in 1:N) {
         XYZ <- xyz[group %in% Groups[j], ]
         XYZ <- JRWToolBox::sort.f(XYZ, 3, rev = TRUE)
-        if(verbose) cat(paste0("\n\nGroup = ", Groups[j], "; Color = ", fill.col[j %r1% length(fill.col)], " with alpha level = ", fill.col.alpha, "\n"))
+        if(verboseGroup) cat(paste0("\n\nGroup = ", Groups[j], "; Color = ", fill.col[j %r1% length(fill.col)], " with alpha level = ", fill.col.alpha, "\n"))
         XYZ[,1] <- XYZ[,1] + xDelta[j]
         if(PCH){
             # Alpha level < 1 made the PNG output very slow????
@@ -95,12 +104,12 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
             # cat("\n", c(nrow(XYZ[XYZ[, 3] %in% 0, 1:2]), nrow(XYZ[TF, 1:2]), min(XYZ[TF, 3]), max(XYZ[TF, 3])), "\n")
         } else {
             for (i in 1:nrow(XYZ)) {
-               if(verbose) JRWToolBox::bar(i, nrow(XYZ)) 
+               if(verboseTimeBar) JRWToolBox::bar(i, nrow(XYZ)) 
                if (XYZ[i, 3] %in% 0 & cross.cex > 0) 
                    points(XYZ[i, 1], XYZ[i, 2], pch = 3, cex = cross.cex, col = cross.col[j %r1% length(cross.col)], lwd = 1)
                
                if (XYZ[i, 3] %>>% 0)
-                    JRWToolBox::circle.f(XYZ[i, 1], XYZ[i, 2], XYZ[i, 3] * Extra.Group.Size[j], adj = adj, fill.col = fill.col.a[j %r1% length(fill.col.a)], 
+                   JRWToolBox::circle.f(XYZ[i, 1], XYZ[i, 2], XYZ[i, 3] * Extra.Group.Size[j], adj = adj, fill.col = fill.col.a[j %r1% length(fill.col.a)], 
                         lwd = border.lwd[j %r1% length(border.lwd)], border.col = border.col[j %r1% length(border.col)], ...)
                        
                if(center.points)    
@@ -109,7 +118,9 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
         }
     }
     if (legend) {
-        pretVec <- pretty(c(0, max(xyzRaw[, 3], na.rm = TRUE)))
+        if(is.null(maxZ))
+            maxZ <- max(xyzRaw[, 3], na.rm = TRUE)
+        pretVec <- pretty(c(0, maxZ))
         Usr <- par()$usr
         Np <- length(pretVec)
         if (Np <= 4) {
@@ -124,7 +135,7 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
         }
         text(Usr[1] + (legLoc[1] + 0.05) * (Usr[2] - Usr[1]), Usr[3] + legLoc[2] * (Usr[4] - Usr[3]), legUnits, cex = ifelse(nchar(legUnits) < 20, 0.9, 0.7))
         if(cross.cex > 0) {
-            text(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.03) * (Usr[4] - Usr[3]), "+")
+            text(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.03) * (Usr[4] - Usr[3]), "+", col = cross.col, cex = 2)
             text(Usr[1] + (legLoc[1] + 0.125) * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.03) * (Usr[4] - Usr[3]), format(0, nsmall = legNsmall), adj = 1, cex = 0.9)
         }    
        #   circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.075) * (Usr[4] - Usr[3]), maxsize * sqrt(Small)/max(xyzSqrt[, 3], na.rm = T), fill.col = col.alpha(legCol, legAlpha))
@@ -134,18 +145,19 @@ plot.bubble.zero.cross <- function (xyzRaw, group = rep("A", nrow(xyzRaw)), Grou
        #   circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.175) * (Usr[4] - Usr[3]), maxsize * sqrt(Large)/max(xyzSqrt[, 3], na.rm = T), fill.col = col.alpha(legCol, legAlpha))
        #   text(Usr[1] + (legLoc[1] + 0.125) * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.175) * (Usr[4] - Usr[3]), format(Large, nsmall = legNsmall), adj = 1, cex = 0.9)
              
-        circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.075) * (Usr[4] - Usr[3]), maxsize * ifelse(Circle.Area.Prop.to.Z, sqrt(Small), Small)/max(xyzSqrt[, 3], na.rm = T), fill.col = col.alpha(legCol, legAlpha))
+        circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.075) * (Usr[4] - Usr[3]), maxsize * ifelse(Circle.Area.Prop.to.Z, sqrt(Small/maxZ), Small/maxZ), fill.col = col.alpha(legCol, legAlpha))
         text(Usr[1] + (legLoc[1] + 0.125) * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.075) * (Usr[4] - Usr[3]), format(Small, nsmall = legNsmall), adj = 1, cex = 0.9)
         
-        circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.115) * (Usr[4] - Usr[3]), maxsize * ifelse(Circle.Area.Prop.to.Z, sqrt(Mid), Mid)/max(xyzSqrt[, 3], na.rm = T), fill.col = col.alpha(legCol, legAlpha))
+        circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.115) * (Usr[4] - Usr[3]), maxsize * ifelse(Circle.Area.Prop.to.Z, sqrt(Mid/maxZ), Mid/maxZ), fill.col = col.alpha(legCol, legAlpha))
         text(Usr[1] + (legLoc[1] + 0.125) * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.115) * (Usr[4] - Usr[3]), format(Mid, nsmall = legNsmall), adj = 1, cex = 0.9)
         
-        circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.175) * (Usr[4] - Usr[3]), maxsize * ifelse(Circle.Area.Prop.to.Z, sqrt(Large), Large)/max(xyzSqrt[, 3], na.rm = T), fill.col = col.alpha(legCol, legAlpha))
+        circle.f(Usr[1] + legLoc[1] * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.175) * (Usr[4] - Usr[3]), maxsize * ifelse(Circle.Area.Prop.to.Z, sqrt(Large/maxZ), Large/maxZ), fill.col = col.alpha(legCol, legAlpha))
         text(Usr[1] + (legLoc[1] + 0.125) * (Usr[2] - Usr[1]), Usr[3] + (legLoc[2] - 0.175) * (Usr[4] - Usr[3]), format(Large, nsmall = legNsmall), adj = 1, cex = 0.9)
         
     }
     invisible()
 }
+
 
 
 
