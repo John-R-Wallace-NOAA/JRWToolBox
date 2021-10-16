1f8dac4d3cae6baeb494572f04a9626f626cbb84
John Wallace <John.Wallace@noaa.gov>
2021-10-15 17:29:51 -0700

    Using scale() properly makes the lnorm() version
    exactly equal to Bolker's version on the reference.

--- R/rLnorm.R
+++ R/rLnorm.R
@@ -1,9 +1,9 @@
-rLnorm <- function(n, m, sd) {
+rLnorm <- function(n, m, sd, verbose = FALSE) {
 
     '  # https://stackoverflow.com/questions/56821688/sample-a-lognormal-distribution-to-an-exact-mean-and-sd  '
-    
-    m2 <- log(m^2 / sqrt(sd^2 + m^2))
-    sd2 <- sqrt(log(1 + (sd^2 / m^2)))
-    cat(paste0('\nmeanlog = ', m2, '\n\nsdlog = ', sd2, "\n\n"))
-    scale(exp(sd2*rnorm(n) + m2)) * sd + m
+
+   if(verbose)
+     cat(paste0('\nmeanlog = ', log(m^2 / sqrt(sd^2 + m^2)), '\n\nsdlog = ', sqrt(log(1 + (sd^2 / m^2))), "\n\n"))
+       
+   scale(rlnorm(n, meanlog = log(m^2 / sqrt(sd^2 + m^2)), sdlog = sqrt(log(1 + (sd^2 / m^2))))) * sd + m
 }
