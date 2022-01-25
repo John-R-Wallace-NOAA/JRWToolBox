716ad3e02e4e46d12774d71b853c6f0ce8e99f91
John Wallace <John.Wallace@noaa.gov>
2022-01-24 17:37:57 -0800

    Now with text_for_copy_paste option

--- R/setWd.R
+++ R/setWd.R
@@ -1,13 +1,10 @@
 
-
-
-setWd <- function () 
+setWd <- function (text_for_copy_paste = FALSE) 
 {
-    '  # After filePath() puts out the correct path, setwd() is used to change the working directory and getwd() verifies '
+    "  # After filePath() returns out the machine dependent correct path, setwd() is used to change the working directory and getwd() verifies "
     setwd(JRWToolBox::filePath())
-    getwd()
+    if(text_for_copy_paste)
+        cat(paste0('\nsetwd("', getwd(), '")\n\n'))
+    else
+        print(getwd()) 
 }
-
-
- 
- 
