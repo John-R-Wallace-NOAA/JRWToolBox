6ebdd7c13258a4b450f6446f60ad4b5a99dd934c
John Wallace <John.Wallace@noaa.gov>
2022-02-15 17:54:24 -0800

    Added example in the comments

--- R/orderedCharAsNum.R
+++ R/orderedCharAsNum.R
@@ -1,5 +1,10 @@
 orderedCharAsNum <- function(x) {
 
+     '  # (z <- paste0(c(1, 103, 4, 7, 112), "_ZZ"))  '
+     '  # gsub("\\D+", " ", z)  '
+     '  # sortCharAsNum(z)  '
+     '  # orderedCharAsNum(z)  '
+     
      sortCharAsNum <- function(y) {
 
         y[order(as.numeric(gsub("\\D+", " ", y)))]
