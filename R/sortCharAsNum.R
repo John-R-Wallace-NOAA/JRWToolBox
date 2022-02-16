58841e4594b3762775d45260a0016006fa5b694c
John Wallace <John.Wallace@noaa.gov>
2022-02-15 18:00:34 -0800

    Quotes fix

--- R/sortCharAsNum.R
+++ R/sortCharAsNum.R
@@ -2,9 +2,9 @@
 
 sortCharAsNum <- function(x) {
 
-   '  # (z <- paste0(c(1, 103, 4, 7, 112), "_ZZ"))  '
-   '  # gsub("\\D+", " ", z)  '
-   '  # sortCharAsNum(z)  '
+    "  # (z <- paste0(c(1, 103, 4, 7, 112), '_ZZ'))  "
+    "  # gsub('\\D+', ' ', z)  "
+    "  # sortCharAsNum(z)  "
     
     x[order(as.numeric(gsub("\\D+", " ", x)))]
 }  
