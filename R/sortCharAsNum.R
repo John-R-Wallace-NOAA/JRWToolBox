321081b2dbcd2c34f1614ba53263cc27a2258047
John Wallace <John.Wallace@noaa.gov>
2022-02-15 17:53:12 -0800

    Update sortCharAsNum.R

--- R/sortCharAsNum.R
+++ R/sortCharAsNum.R
@@ -2,6 +2,10 @@
 
 sortCharAsNum <- function(x) {
 
+   '  # (z <- paste0(c(1, 103, 4, 7, 112), "_ZZ"))  '
+   '  # gsub("\\D+", " ", z)  '
+   '  # sortCharAsNum(z)  '
+    
     x[order(as.numeric(gsub("\\D+", " ", x)))]
 }  
 
