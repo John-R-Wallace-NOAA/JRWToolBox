ec3df4ca10d9086cebc88707f02e0542bed080a7
John Wallace <John.Wallace@noaa.gov>
2022-02-16 17:45:41 -0800

    Cases of pattern now tried by default

--- R/Ls.R
+++ R/Ls.R
@@ -1,49 +1,77 @@
-Ls <-function (pattern = "[!.]*", pos = 1, all = FALSE, long = FALSE, open = FALSE, fix = FALSE, ...) 
-{
-    if (all) 
-        pos <- 1:length(search())
-    
-   if (!(is.character(substitute(pattern)))) {
-       pattern <- deparse(substitute(pattern))
-   }
-   if (long) {
-       for( i in pos) {
-          cat("\n")
-          print(ll(pos = i, pattern = pattern))
-          cat("\n")
-        }
-        return(invisible())
-    }
-   else {
-        out <- lapply(as.list(pos), objects, pat = pattern, ...)
-        names(out) <- search()[pos]
-   }
-   xlen <- length(out)
-   this <- paste("[[", names(out), "]]", sep = "")
-   for (i in 1:xlen) {
-       if (length(out[[i]]) > 0) {
-           if (is.data.frame(out[[i]]) == TRUE) {
-             if (dim(out[[i]])[1] > 0) {
-               cat(this[i], ":\n", sep = "")
-               print(out[[i]], prefix = this[i], q = FALSE)
-               cat("\n")
-             }
-           }
-           else {
-             cat(this[i], ":\n", sep = "")
-             print(out[[i]], prefix = this[i], q = FALSE)
+
+Ls <- function(pattern = "[!.]*", pos = 1, cases = TRUE, all = FALSE, long = FALSE, open = FALSE, fix = FALSE, ...) {
+
+   Lss <- function (pattern = "[!.]*", pos = 1, all = FALSE, long = FALSE, open = FALSE, fix = FALSE, ...) {
+   
+       if (all) 
+           pos <- 1:length(search())
+     
+      if (long) {
+          for( i in pos) {
+             cat("\n")
+             print(ll(pos = i, pattern = pattern))
              cat("\n")
            }
+           return(invisible())
        }
+      else {
+           out <- lapply(as.list(pos), objects, pat = pattern, ...)
+           names(out) <- search()[pos]
+      }
+      xlen <- length(out)
+      this <- paste("[[", names(out), "]]", sep = "")
+      for (i in 1:xlen) {
+          if (length(out[[i]]) > 0) {
+              if (is.data.frame(out[[i]])) {
+                if (dim(out[[i]])[1] > 0) {
+                  cat(this[i], ":\n", sep = "")
+                  print(out[[i]], prefix = this[i], q = FALSE)
+                  cat("\n")
+                }
+              }
+              else {
+                cat(this[i], ":\n", sep = "")
+                print(out[[i]], prefix = this[i], q = FALSE)
+                cat("\n")
+              }
+          }
+      }
+      if (open) {
+           for (i in 1:length(out)) {
+               cat("\n\n***", out[i], "***\n")
+               print(eval(parse(text = out[i])))
+           }
+       }
+       if (length(out) == 1 & fix) 
+           assign(substitute(out), edit(eval(parse(text = out))), pos = 1)
+         
+       invisible(out)
    }
-   if (open) {
-        for (i in 1:length(out)) {
-            cat("\n\n***", out[i], "***\n")
-            print(eval(parse(text = out[i])))
-        }
+
+   if (!(is.character(substitute(pattern)))) 
+        pattern <- deparse(substitute(pattern))
+  
+    if(pattern == "[!.]*")
+       Lss(pattern, pos = pos, all = all, long = long, open = open, fix = fix, ...)
+  
+    else {
+       if(cases) {
+          Pat <- tolower(pattern)
+          cat("\n--", Pat, "--\n")
+          Lss(Pat, pos = pos, all = all, long = long, open = open, fix = fix, ...)
+          
+          Pat <- JRWToolBox::casefold.f(pattern, first.letter.cap = TRUE)
+          cat("\n--", Pat, "--\n")
+          Lss(Pat, pos = pos, all = all, long = long, open = open, fix = fix, ...)
+          
+          Pat <- toupper(pattern)
+          cat("\n--", Pat, "--\n")
+          Lss(Pat, pos = pos, all = all, long = long, open = open, fix = fix, ...)
+       }
+       else
+         Lss(pattern, pos = pos, all = all, long = long, open = open, fix = fix, ...)
+       
     }
-    if (length(out) == 1 & fix == TRUE) 
-        assign(substitute(out), edit(eval(parse(text = out))), 
-            pos = 1)
-    invisible(out)
 }
+
+
--- R/panel.conf.pred.band.R
+++ R/panel.conf.pred.band.R
@@ -1,93 +1,82 @@
-' if(F) { '
-'  x_1:100 '
-'  y_3+ 1.3*x + 0.02*x^2 + rnorm(100, 0, 25) '
-'  xyplot(y~x,panel=panel.conf.pred.band) '
-'  } '
 
-panel.conf.pred.band<-
-function(x, y, type = "p", cex = plot.symbol$cex, pch = plot.symbol$pch, font = 
-	plot.symbol$font, lwd = plot.line$lwd, lty = plot.line$lty, col = if(type ==
-	"l") plot.line$col else plot.symbol$col, ...)
-{
+panel.conf.pred.band <- function(x, y, type = "p", cex = plot.symbol$cex, pch = plot.symbol$pch, font = plot.symbol$font, 
+         lwd = plot.line$lwd, lty = plot.line$lty, col = if(type ==	"l") plot.line$col else plot.symbol$col, ...) {
+     
+     if(FALSE) {
+     
+        "  DATE WRITTEN:  03 May 2001      LAST REVISED:   16 Feb 2022  "
+        "  AUTHOR:  John R. Wallace (john.wallace@noaa.gov)  "
+        
+        "  Confidence Bands and prediction bands as set forth in Applied regression analysis  "
+        "  and other multivariable methods by Kleinbaum, D.G. and Kupper, L.L. 1978.  "
+        "  Duxbuxy Press, North Scituate, Massachusetts.  "
+        
+        "  Example  "
+        dev.new(width = 800, height = 500)
+        x <- 1:100
+        y <- 3 + 1.3*x + 0.02*x^2 + rnorm(100, 0, 25)
+        xyplot(y ~ x, panel = panel.conf.pred.band)
+     
+     }
+
 	if(type == "l") {
 		plot.line <- trellis.par.get("plot.line")
 		lines(x, y, lwd = lwd, lty = lty, col = col, type = type, ...)
 	}
 	else {
-		""
-		"DATE WRITTEN:  03 May 2001      LAST REVISED:   04 May 2001"
-		"AUTHOR:  John R. Wallace (john.wallace@noaa.gov)"
-		""
-		"Confidence Bands and prediction bands as set forth in Applied regression analysis"
-		"and other multivariable methods by Kleinbaum, D.G. and Kupper, L.L. 1978."
-		"Duxbuxy Press, North Scituate, Massachusetts."
-		""
-		alpha <- 0.1
-		""
+		
+		alpha <- 0.05
+		
 		x.ok <- x[!(is.na(x) | is.na(y))]
 		y.ok <- y[!(is.na(x) | is.na(y))]
-		lm.fit <- lm(y.ok ~ x.ok, na = na.omit)
-		""
+		glm.fit <- glm(y.ok ~ x.ok, na = na.omit)
+		
 		"Prediction Bands"
-		""
+		
 		x.ok.min <- min(x.ok)
 		x.ok.max <- max(x.ok)
 		fudge <- (x.ok.max - x.ok.min)/40
 		assign("x.new", c(x.ok.min - fudge, seq(x.ok.min, x.ok.min, 
 			length = 30), x.ok.max + fudge), pos = 1)
 		assign("x.ok", x.ok, pos = 1)
-		lm.pred <- predict.gam(lm.fit, data.frame(x.ok = x.new), type = 
-			"response", se.fit = T)
-		""
-		S2yx <- sum(resid(lm.fit)^2)/lm.fit$df.residual
-		pred.se <- (S2yx * (lm.pred$se.fit^2/S2yx + 1))^0.5
-		""
-		pred.lower <- cbind(x.new, lm.pred$fit - pred.se * qt(1 - alpha/2,
-			lm.pred$df))[order(x.new),  ]
-		pred.upper <- cbind(x.new, lm.pred$fit + pred.se * qt(1 - alpha/2,
-			lm.pred$df))[order(x.new),  ]
-		if(T)
-			polygon(c(pred.lower[, 1], rev(pred.upper[, 1])), c(
-				pred.lower[, 2], rev(pred.upper[, 2])), col = 14)
-		if(F) {
-			lines(pred.lower[, 1], pred.lower[, 2], col = 1)
-			lines(pred.upper[, 1], pred.upper[, 2], col = 1)
-		}
-		""
-		"Confidence Bands"
-		""
-		lm.pred <- predict.gam(lm.fit, data.frame(x.ok = x.ok), type = 
-			"response", se.fit = T)
-		conf.lower <- cbind(x.ok, lm.pred$fit - lm.pred$se.fit * qt(1 - 
-			alpha/2, lm.pred$df))[order(x.ok),  ]
-		conf.upper <- cbind(x.ok, lm.pred$fit + lm.pred$se.fit * qt(1 - 
-			alpha/2, lm.pred$df))[order(x.ok),  ]
+		glm.pred <- gam::predict.Gam(glm.fit, data.frame(x.ok = x.new), type = "response", se.fit = T)
+		
+		S2yx <- sum(resid(glm.fit)^2)/glm.fit$df.residual
+		pred.se <- (S2yx * (glm.pred$se.fit^2/S2yx + 1))^0.5
+		
+		pred.lower <- cbind(x.new, glm.pred$fit - pred.se * qt(1 - alpha/2, glm.fit$df.residual))[order(x.new),  ]
+		pred.upper <- cbind(x.new, glm.pred$fit + pred.se * qt(1 - alpha/2, glm.fit$df.residual))[order(x.new),  ]
 		if(TRUE)
-			polygon(c(conf.lower[, 1], rev(conf.upper[, 1])), c(
-				conf.lower[, 2], rev(conf.upper[, 2])), col = 3)
+			lpolygon(c(pred.lower[, 1], rev(pred.upper[, 1])), c(pred.lower[, 2], rev(pred.upper[, 2])), col = 'violet') 
 		if(FALSE) {
-			lines(conf.lower[, 1], conf.lower[, 2], col = 1)
-			lines(conf.upper[, 1], conf.upper[, 2], col = 1)
+			llines(pred.lower[, 1], pred.lower[, 2])
+			llines(pred.upper[, 1], pred.upper[, 2])
 		}
-		""
+		
+		"Confidence Bands"
+		glm.pred <- gam::predict.Gam(glm.fit, data.frame(x.ok = x.ok), type = "response", se.fit = T)
+		conf.lower <- cbind(x.ok, glm.pred$fit - glm.pred$se.fit * qt(1 - alpha/2, glm.fit$df.residual))[order(x.ok),  ]
+		conf.upper <- cbind(x.ok, glm.pred$fit + glm.pred$se.fit * qt(1 - alpha/2, glm.fit$df.residual))[order(x.ok),  ]
+		if(TRUE)
+			lpolygon(c(conf.lower[, 1], rev(conf.upper[, 1])), c(conf.lower[, 2], rev(conf.upper[, 2])), col = 'green2')
+		if(FALSE) {
+			llines(conf.lower[, 1], conf.lower[, 2])
+			llines(conf.upper[, 1], conf.upper[, 2])
+		}
+		
 		plot.symbol <- trellis.par.get("plot.symbol")
-		if(T)
-			points(x, y, pch = pch, font = font, cex = cex, col = 1, 
-				type = type, ...)
-		else points(x, y, pch = pch, font = font, cex = cex, col = col, 
-				type = type, ...)
-		""
+	    lpoints(x, y, pch = pch, font = font, cex = cex, type = type, col = 'black', ...)
+		
+		
 		"Regresssion Line"
-		""
-		abline(lm.fit$coef, lwd = 1)
-		""
+		panel.abline(glm.fit$coef, lwd = 1)
+		
 		"Lowess Line"
-		""
-		lowess.line(x, y, lty = 3, col = 2, lwd = 2)
-		""
+		JRWToolBox::panel.Lowess(x, y, lty = 3, col = 'indianred1', lwd = 2)
+		
 		"1-1 Line"
-		""
-		abline(0, 1, lty = 4)
+		" panel.abline(0, 1, lty = 4) "
 	}
 }
+
 
