b4964ecf64fed56a24b99cd1348a19ad7fb96d18
John Wallace <John.Wallace@noaa.gov>
2022-05-12 14:24:22 -0700

    Clean up

--- R/llowess.line.R
+++ R/llowess.line.R
@@ -1,6 +1,5 @@
 llowess.line <- function (x, y, smoothing.param = 2/3, ...) 
 {
     tmp <- na.omit(cbind(x, y))
-    llines(stats::lowess(tmp[, 1], tmp[, 2], f = smoothing.param), 
-        ...)
+    llines(stats::lowess(tmp[, 1], tmp[, 2], f = smoothing.param), ...)
 }
