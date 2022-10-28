09d46317c61b632fa3941cb540337a55338c02e9
John Wallace <John.Wallace@noaa.gov>
2022-10-27 20:51:40 -0700

    Finally switching to using local_and_remote_SHA()

--- R/gitHub_SHA.R
+++ R/gitHub_SHA.R
@@ -1,7 +1,5 @@
-gitHub_SHA <- function (repo, ref = "master", quiet = FALSE)  {
-    SHA <- remotes:::remote_sha(lapply(repo, remotes:::github_remote, 
-        username = NULL, ref = ref, subdir = NULL, auth_token = remotes:::github_pat(quiet), 
-        host = "https://api.github.com")[[1]])
+gitHub_SHA  <- function (repo, ref = "master", quiet = FALSE)  {
+    SHA <- JRWToolBox::local_and_remote_SHA("John-R-Wallace-NOAA/JRWToolBox")[[2]]
     cat("\nSHA:", SHA, "from reference:", ref)
     cat("\n\nCurrent date and time and the call to revert to this Commit in the future:\n\n", 
         as.character(Sys.time()), "\nremotes::install_github('", 
@@ -9,5 +7,3 @@ gitHub_SHA <- function (repo, ref = "master", quiet = FALSE)  {
     invisible(list(Date = Sys.time(), Call = paste("remotes::install_github('", 
         repo, "', ref = '", SHA, "')", sep = "")))
 }
-
-
