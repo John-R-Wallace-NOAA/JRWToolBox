117671e01c78ea114be4e7fb62be03500e2eb0b0
John Wallace <John.Wallace@noaa.gov>
2023-01-13 20:27:23 -0800

    Now uses predict.lowess() via lowess.line()

--- R/plot.lowess.R
+++ R/plot.lowess.R
@@ -1,30 +1,27 @@
-plot.lowess <- 
-function (x, y, smoothing.param = 2/3, type = "p", jitter.x = F, factor = 1, line.col = "green", 
-           xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...) 
-{
+                      
+plot.lowess <- function (x, y, smoothing.param = 2/3, type = "p", jitter.x = FALSE, factor = 1, line.col = "green", 
+                method = c("fmm", "periodic", "natural", "monoH.FC", "hyman"), ties = mean, 
+                xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...) {
+                
    if(jitter.x)
       plot(jitter(x, factor), y, type = type, xlab = xlab, ylab = ylab, ...)
    else
       plot(x, y, type = type, xlab = xlab, ylab = ylab, ...)
 
-   lowess.line(x, y, smoothing.param = smoothing.param, col = line.col)
+   JRWToolBox::lowess.line(x, y, smoothing.param = smoothing.param, col = line.col, method = method, ties = ties)
 }
 
-
-plot.lowess.range <- 
-function (x, y, smoothing.param = c(1/6, 1/3, 1/2, 2/3, 5/6), type = "p", jitter.x = F, factor = 1, line.col = "green", 
-    ...) 
-
-{
+                      
+plot.lowess.range <- function (x, y, smoothing.param = c(1/6, 1/3, 1/2, 2/3, 5/6), type = "p", jitter.x = FALSE, factor = 1, 
+                line.col = rainbow(length(smoothing.param)), method = c("fmm", "periodic", "natural", "monoH.FC", "hyman"), 
+                ties = mean, xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...)   {
+                
    if(jitter.x)
       plot(jitter(x, factor), y, type = type, xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...)
    else
       plot(x, y, type = type, xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...)
 
    for (i in 1:length(smoothing.param))
-      lowess.line(x, y, smoothing.param = smoothing.param[i], col = rainbow(length(smoothing.param))[i])
-  
+      JRWToolBox::lowess.line(x, y, smoothing.param = smoothing.param[i], col = line.col[i])
 }
-
-
 
