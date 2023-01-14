47f1d7d16308c0370681e3ef40d02f65f13bec45
John Wallace <John.Wallace@noaa.gov>
2023-01-13 20:28:40 -0800

    Now uses JRWToolBox::predict.lowess()

--- R/lowess.line.R
+++ R/lowess.line.R
@@ -1,5 +1,9 @@
-lowess.line <- function(x, y, smoothing.param = 2/3, ...)
-{
+
+lowess.line <- function(x, y, smoothing.param = 2/3, method = c("fmm", "periodic", "natural", "monoH.FC", "hyman"), ties = mean, ...) {
+
        tmp <- na.omit(cbind(x, y))
-       lines(stats::lowess(tmp[, 1], tmp[, 2], f = smoothing.param), ...)
-} 
+       lo <- stats::lowess(tmp[, 1], tmp[, 2], f = smoothing.param)
+       predictLo <- JRWToolBox::predict.lowess(lo, method = c("fmm", "periodic", "natural", "monoH.FC", "hyman"), ties = mean)
+       j <- order(lo$x)
+       lines(lo$x[j], predictLo[j], ...)
+}
