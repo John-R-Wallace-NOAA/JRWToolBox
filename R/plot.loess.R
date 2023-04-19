4f7372e4c85d7394d3bb999767f51c5d73de05ed
John Wallace <John.Wallace@noaa.gov>
2023-04-19 15:54:28 -0700

    Took off JRWToolBox:: so will work stand alone
    with loess.line() also available.

--- R/plot.loess.R
+++ R/plot.loess.R
@@ -7,7 +7,7 @@ plot.loess <- function (x, y, span = 0.75, type = "p", jitter.x = F, factor = 1,
    else
       plot(x, y, type = type, xlab = xlab, ylab = ylab, ...)
 
-   JRWToolBox::loess.line(x, y, span = span, col = line.col)
+   loess.line(x, y, span = span, col = line.col)
 }
 
 
@@ -20,6 +20,6 @@ plot.loess.range <- function (x, y, span = c(1/6, 1/3, 1/2, 2/3, 3/4, 5/6, 1, 2,
       plot(x, y, type = type, xlab = xlab, ylab = ylab, ...)
 
    for (i in 1:length(span))
-      JRWToolBox::loess.line(x, y, span = span[i], col = line.col[i])
+      loess.line(x, y, span = span[i], col = line.col[i])
 }
 
