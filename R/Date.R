51e79ce061897b208b6cc3f829334aa48656fdb5
John Wallace <John.Wallace@noaa.gov>
2023-09-27 12:50:08 -0700

    added time option

--- R/Date.R
+++ R/Date.R
@@ -1,6 +1,11 @@
 
-Date <- function (collapse  = "_") 
-{
-    paste(get.subs(sub("  ", " ", date()), " ")[c(3, 2, 5)], 
-        collapse = collapse)
+Date <- function (time = FALSE, collapse  = "_") {
+
+    dateSubs <- get.subs(sub("  ", " ", date()), " ")
+    
+    if(time) {
+       timeSubs <- gsub(":", collapse, dateSubs[4])
+       paste0(paste(dateSubs[c(3, 2, 5)], collapse = collapse), collapse, timeSubs)
+    } else
+       paste(dateSubs[c(3, 2, 5)], collapse = collapse)
 }
