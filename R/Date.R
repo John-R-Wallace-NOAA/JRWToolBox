41eb29ade2cff1ab8197f00617c23f8b24425088
John Wallace <John.Wallace@noaa.gov>
2023-09-27 14:25:59 -0700

    Update Date.R

--- R/Date.R
+++ R/Date.R
@@ -1,10 +1,10 @@
 
-Date <- function (time = FALSE, collapse  = "_") {
+Date <- function (Time = FALSE, collapse  = "_") {
     
     '  # Note: To get the date correct, 2 spaces down to 1 space is needed when there is a single digit day of month  ' 
     dateSubs <- get.subs(sub("  ", " ", date()), " ")
     
-    if(time) {
+    if(Time) {
        timeSubs <- gsub(":", collapse, dateSubs[4])
        paste0(paste(dateSubs[c(3, 2, 5)], collapse = collapse), collapse, timeSubs)
     } else
