cdc75a19671848e62ca81a02233bb79472905451
John Wallace <John.Wallace@noaa.gov>
2023-10-02 15:45:59 -0700

    Update extractRData.R

--- R/extractRData.R
+++ R/extractRData.R
@@ -1,8 +1,6 @@
 extractRData <- function(object, file) {
       '   # Function for extracting an object from a .RData file created by Rs save() command   '
       '   # Inputs: object nam, RData file   '
-      if(!is.character(object))
-         object = deparse(substitute(object))
       e <- new.env()
       base::load(file=file, envir = e)
       return(get(object, envir = e, inherits = FALSE))
