85d584965cb0caf1ef9c7a3af8f73c8cede8b855
John Wallace <John.Wallace@noaa.gov>
2023-10-17 12:53:34 -0700

    Update Str.R

--- R/Str.R
+++ R/Str.R
@@ -1,11 +1,20 @@
-Str <- function(object, N = 4, ...) {
-   str(object, ...)
+Str <- function(object, N = 4, ncols = 7, ...) {
+   
    cat("\n\n")
-   if(is.list(object) & !is.data.frame(object))
-       lapply(object, head, 4)
-   else 
+   
+    if(is.list(object) & is.data.frame(object)) {
+       Cols <- min(c(7, ncol(object)))
+       str(object[, 1:Cols], ...)
+       lapply(object[, 1:Cols], head, N)
+   }
+   
+   if(is.list(object) & !is.data.frame(object)) {
+       str(object, ...)
+       lapply(object, head, N)
+   }
+   
+   if(!is.list(object) & !is.data.frame(object)) {
+       str(object, ...)
        head(object, N)
+   }    
 }
-
-
-
