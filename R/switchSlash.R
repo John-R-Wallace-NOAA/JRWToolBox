3cc45ea9ae690be4461aaab0f950b504b65cd167
John Wallace <John.Wallace@noaa.gov>
2023-10-16 17:21:44 -0700

    Update switchSlash.R
    Now also converts '\\' (which works in R) to '/'.

--- R/switchSlash.R
+++ R/switchSlash.R
@@ -2,16 +2,19 @@
 
 switchSlash <- function (backSlash = readClipboard()) 
 {
-   forwardSlash <- gsub("\\\\", "/", backSlash)
-   cat(forwardSlash, file = "clipboard")
-   forwardSlash
+    forwardSlash <- gsub("//", "/", gsub("\\\\", "/", backSlash))
+    cat(forwardSlash, file = "clipboard")
+    forwardSlash
 }
 
 sS <- function (backSlash = readClipboard()) 
 {
-   forwardSlash <- gsub("\\\\", "/", backSlash)
-   cat(forwardSlash, file = "clipboard")
-   forwardSlash
+    forwardSlash <- gsub("//", "/", gsub("\\\\", "/", backSlash))
+    cat(forwardSlash, file = "clipboard")
+    forwardSlash
 }
+
+
+
 
 
