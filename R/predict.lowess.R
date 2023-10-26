a2da745466ae50635436e598d6ebd51427b3d288
John Wallace <John.Wallace@noaa.gov>
2023-10-26 14:54:08 -0700

    If the x variable is descending, then it has to be called explictly, i.e. predict.lowess(lo.car, cars$speed)

--- R/predict.lowess.R
+++ R/predict.lowess.R
@@ -1,10 +1,11 @@
-
-predict.lowess <- function(loFit, newdata = loFit$x, method = c("fmm", "periodic", "natural", "monoH.FC", "hyman"), ties = mean) {  
+predict.lowess <- 
+function(loFit, newdata = loFit$x, method = c("fmm", "periodic", "natural", "monoH.FC", "hyman"), ties = mean) {  
       "  "
       "  # dev.new()   "
-      "  # plot.lowess(cars$speed, cars$dist)   "
+      "  # JRWToolBox::plot.lowess(cars$speed, cars$dist)   "
   
       "  # lo.car <- lowess(cars$speed, cars$dist)   "
+      "  # If the x variable is descending, then it has to be called explictly, i.e. predict.lowess(lo.car, cars$speed)  "
       "  # points(cars$speed, predict.lowess(lo.car), col = 'dodgerblue', pch = 19)    "
   
       "  # x.new <- c(5.3, 6.8, 20.5, 25.2)   "
@@ -12,5 +13,3 @@ predict.lowess <- function(loFit, newdata = loFit$x, method = c("fmm", "periodic
       "  "
    (stats::splinefun(loFit, method = method, ties = ties))(newdata)
 }
- 
- 
