5ca21db3d75ccab6efd2b81218325f1a734c3240
John Wallace <John.Wallace@noaa.gov>
2023-12-15 17:50:59 -0800

    Update gPlot.R

--- R/gPlot.R
+++ R/gPlot.R
@@ -1,11 +1,11 @@
 
-gPlot <- function(data, x, y, plotRegionColor = rgb(235, 235, 235, maxColorValue = 255), ...) {
-   plot(c(0, 1), c(0, 1), xlab = "", ylab = "", type = "n", xaxt = "n", yaxt = "n", bty = "n")
+gPlot <- function(data, x, y, plotRegionColor = rgb(235, 235, 235, maxColorValue = 255), col = 'black', pch = 1, grid = TRUE, ...) {
+   plot(y ~ x, data = data, xlab = "", ylab = "", type = "n", xaxt = "n", yaxt = "n", bty = "n")
    rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = plotRegionColor) 
+   if(grid)
+      grid(col = 'white', lty = 1, lwd = 1)
    par(new = TRUE)
-'  # plot(y ~ x, data = data, xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...) # Does not work ????  '
-'  # Has to be saved to an R object to work!  '
-   xlab <- deparse(substitute(x)) 
-   plot(y ~ x, data = data, xlab = xlab, ylab = deparse(substitute(y)), ...)
-   grid(col = 'white', lty = 1, lwd = 1)
+   # plot(y ~ x, data = data, xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...) # Does not work ?????????????
+   xlab <- deparse(substitute(x)) # Has to saved to an R object to work???
+   plot(y ~ x, data = data, xlab = xlab, ylab = deparse(substitute(y)), col = col, pch = pch, ...)
 } 
