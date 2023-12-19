8df459299ac6ba6377b4f70e8655b46c8df304a4
John Wallace <John.Wallace@noaa.gov>
2023-12-19 13:58:12 -0800

    Bug fixes

--- R/gPlot.R
+++ R/gPlot.R
@@ -1,11 +1,10 @@
 
 gPlot <- function(data, x, y, plotRegionColor = rgb(235, 235, 235, maxColorValue = 255), col = 'black', pch = 1, grid = TRUE, ...) {
-   plot(y ~ x, data = data, xlab = "", ylab = "", type = "n", xaxt = "n", yaxt = "n", bty = "n")
+   # plot(y ~ x, data = data, xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...) # Does not work for xlab, but it does for ylab????
+   xlab <- deparse(substitute(x)) # xlab has to be saved to an R object to work???
+   plot(y ~ x, data = data, xlab = xlab, ylab = deparse(substitute(y)), type = "n", ...)
    rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = plotRegionColor) 
    if(grid)
       grid(col = 'white', lty = 1, lwd = 1)
-   par(new = TRUE)
-   # plot(y ~ x, data = data, xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...) # Does not work ?????????????
-   xlab <- deparse(substitute(x)) # Has to saved to an R object to work???
-   plot(y ~ x, data = data, xlab = xlab, ylab = deparse(substitute(y)), col = col, pch = pch, ...)
-} 
+   points(y ~ x, data = data, col = col, pch = pch) # Points over grid lines looks better
+}
