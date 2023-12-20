5d0c4a7ff00e241b98773f80cd999a950457c234
John Wallace <John.Wallace@noaa.gov>
2023-12-19 18:27:44 -0800

    major update

--- R/gPlot.R
+++ R/gPlot.R
@@ -1,10 +1,23 @@
 
-gPlot <- function(data, x, y, plotRegionColor = rgb(235, 235, 235, maxColorValue = 255), col = 'black', pch = 1, grid = TRUE, ...) {
-   # plot(y ~ x, data = data, xlab = deparse(substitute(x)), ylab = deparse(substitute(y)), ...) # Does not work for xlab, but it does for ylab????
-   xlab <- deparse(substitute(x)) # xlab has to be saved to an R object to work???
-   plot(y ~ x, data = data, xlab = xlab, ylab = deparse(substitute(y)), type = "n", ...)
-   rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = plotRegionColor) 
-   if(grid)
-      grid(col = 'white', lty = 1, lwd = 1)
-   points(y ~ x, data = data, col = col, pch = pch) # Points over grid lines looks better
+ gPlot <- function(data, x.name, y.name, xFunc = NULL, xlab = NULL, ylab = NULL, plotRegionColor = rgb(235, 235, 235, maxColorValue = 255), col = 'black', pch = 1, grid = TRUE, vertLineEachPoint = FALSE, verbose = FALSE, ...) {
+    
+    if(is.null(xFunc))
+         xFunc.name <- NULL
+    else      
+        xFunc.name <- deparse(substitute(xFunc))
+    Formula <- as.formula(paste0(y.name, ' ~ ', xFunc.name, '(', x.name, ')'))
+    if(verbose) {
+        cat("\n\n")
+        print(Formula)
+    }     
+    plot(Formula, data = data, xlab = ifelse(is.null(xlab), ifelse(is.null(xFunc), x.name, paste0(xFunc.name, '(', x.name, ')')), xlab), ylab = ifelse(is.null(ylab), y.name, ylab), type = "n", ...)
+    rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = plotRegionColor) 
+    if(grid)
+       grid(col = 'white', lty = 1, lwd = 1)
+    if(vertLineEachPoint) 
+        abline(v = unique(data[, x.name]), h = 0, col = "grey")
+    if(is.null(xFunc))
+       points(data[, x.name], data[, y.name], col = col, pch = pch) # Points over grid lines looks better
+    else
+       points(do.call(xFunc, list(data[, x.name])), data[, y.name], col = col, pch = pch)
 }
