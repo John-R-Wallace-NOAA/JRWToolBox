fbd72bd2e7ac46cab374ae4ae605a1664bdca307
John Wallace <John.Wallace@noaa.gov>
2024-01-04 16:48:59 -0800

    Update gl.f.R

--- R/gl.f.R
+++ R/gl.f.R
@@ -1,5 +1,5 @@
 gl.f <- function(end, run, length = (end - start + 1) * run, start = 1, seq = start:end) {
-       '  # Unlike gl(), this function does not return a factor  '
-       '  # I wrote in this in S-plus before gl() was part of R.  '
+       '  # Unlike base::gl(), this function does not return a factor  '
+       '  # I wrote this in S-plus before gl() was part of R.  '
 	        rep(rep(seq, len = lr <- length/run), rep(run, lr))
 }
