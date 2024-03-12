be9fca187047b204c108b8c5b6b48d239acbfac9
John Wallace <John.Wallace@noaa.gov>
2024-03-12 12:32:34 -0700

    bug fix

--- R/headTail.R
+++ R/headTail.R
@@ -1,5 +1,5 @@
 
-headTail <- function(x, n_head = 6L, n_tail = n_head, n_col_head = ifelse(nrow(x) > 50L, n_head, NULL), n_col_tail = n_col_head) {
+headTail <- function(x, n_head = 6L, n_tail = n_head, n_col_head = if(ncol(New_Ages) > 50) n_head else NULL, n_col_tail = n_col_head) {
 
     out <- rbind(head(x, n = n_head), tail(x, n = n_tail))
     
@@ -8,10 +8,9 @@ headTail <- function(x, n_head = 6L, n_tail = n_head, n_col_head = ifelse(nrow(x
     
     if(is.null(n_col_head) & !is.null(n_col_tail) ) 
        out <- out[, (ncol(x) - n_col_tail + 1):ncol(x)]
-	   
+           
     if(!is.null(n_col_head) & is.null(n_col_tail) ) 
        out <- out[, 1:n_col_head]
    
    out
 }
-
