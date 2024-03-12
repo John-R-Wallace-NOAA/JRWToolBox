15cfba7ab7cdcbca5325ee7d8da65ce1a7f2b602
John Wallace <John.Wallace@noaa.gov>
2024-03-12 12:03:41 -0700

    default num of cols adjusted to back to rows

--- R/headTail.R
+++ R/headTail.R
@@ -1,5 +1,5 @@
 
-headTail <- function(x, n_head = 6L, n_tail = n_head, n_col_head = ifelse(nrow(x) > 50L, floor(n_head/2), NULL), n_col_tail = n_col_head) {
+headTail <- function(x, n_head = 6L, n_tail = n_head, n_col_head = ifelse(nrow(x) > 50L, n_head, NULL), n_col_tail = n_col_head) {
 
     out <- rbind(head(x, n = n_head), tail(x, n = n_tail))
     
