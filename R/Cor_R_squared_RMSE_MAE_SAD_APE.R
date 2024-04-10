617e8f17f1413959e70f3af192fa12baea987a5b
John Wallace <John.Wallace@noaa.gov>
2024-04-10 16:38:15 -0700

    Update Cor_R_squared_RMSE_MAE_SAD_APE.R

--- R/Cor_R_squared_RMSE_MAE_SAD_APE.R
+++ R/Cor_R_squared_RMSE_MAE_SAD_APE.R
@@ -15,9 +15,10 @@ function(Obs, Pred, na.rm = TRUE, digits = 6) {
              RMSE = sqrt(mean((Pred - Obs)^2, na.rm = na.rm)), 
               MAE = mean(abs(Pred - Obs), na.rm = na.rm),
               SAD = sum(abs(Pred - Obs), na.rm = na.rm),
-			  APE = mean(apply(cbind(ObsZ, PredZ), 1, function(x) 100 * sum(abs(na(x, na.rm = na.rm)  - mean(x, na.rm = na.rm))/mean(x, na.rm = na.rm))/length(na(x, na.rm = na.rm)))),
-              N = length(na(x, na.rm = na.rm))
+			  APE = mean(apply(cbind(ObsZ, PredZ), 1, function(x) 100 * sum(abs(na(x, na.rm = na.rm) - mean(x, na.rm = na.rm))/mean(x, na.rm = na.rm))/length(na(x, na.rm = na.rm))), na.rm = na.rm),
+              N = ifelse(na.rm, nrow(na.omit(cbind(Obs, Pred))), length(Obs))
     ), digits = digits))
-	cat("\n", '   Note: The FSA package appears to add a very small number to the zero values for APE, here the zeros are set to half of the minimum non-zero value.\n\n')
+	cat("\n", '   Note: The FSA package appears to add a very small number to the zero values for APE; here the zeros are set to half of the minimum non-zero value.\n\n')
 	invisible(out)
 }
+
